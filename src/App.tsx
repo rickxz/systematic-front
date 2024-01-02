dc63381107c3d1ca253972911480921c74a3ff93
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-02 16:34:06 -0300

    feat: Add chakra-ui  to project dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './global/react.svg'
-import viteLogo from '/vite.svg'
-import './global/App.css'
+import { Box, Text } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box>
+      <Text color={"Black"} padding={"10%"} fontSize={"32px"} textAlign={"center"}>
+        Olá, Mundo!
+      </Text>
+    </Box>
+  );
 }
 
-export default App
+export default App;
