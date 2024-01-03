4705bcea61c9a47ed9a7748e5b4335d690f69d45
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-03 19:26:39 -0300

    refactor: Improve clarity, naming, and conciseness in NavItem component following Clean Code principles

--- src/components/ui/NavBar/subcomponents/NavItem.tsx
+++ src/components/ui/NavBar/subcomponents/NavItem.tsx
@@ -1,23 +1,23 @@
-import { Flex, Icon, Link, Menu, MenuButton, Text } from "@chakra-ui/react";
+import { Flex, Icon, Menu, MenuButton, Text } from "@chakra-ui/react";
 
-interface INavItem {
+interface INavItemProps {
   navSize: string;
   icon?: React.ElementType;
   title: string;
 }
 
-export default function NavItem({ navSize, icon, title }: INavItem) {
+export default function NavItem({ navSize, icon, title }: INavItemProps): JSX.Element {
+  const isSmallSize = navSize === "small";
+
   return (
-    <Flex mt={30} flexDir={"column"} w={"100%"} align={navSize == "small" ? "center" : "flex-start"}>
+    <Flex mt={30} flexDir="column" w="100%" align={isSmallSize ? "center" : "flex-start"}>
       <Menu placement="right">
-        <Link>
-          <MenuButton>
-            <Flex alignItems={"center"} gap={1.5}>
-              <Icon marginLeft={navSize != "small" ? "10px" : 0} size={"sm"} as={icon} />
-              <Text display={navSize == "small" ? "none" : "flex"}>{title}</Text>
-            </Flex>
-          </MenuButton>
-        </Link>
+        <MenuButton>
+          <Flex alignItems="center" gap={1.5}>
+            <Icon marginLeft={isSmallSize ? 0 : "10px"} size="sm" as={icon} />
+            <Text display={isSmallSize ? "none" : "flex"}>{title}</Text>
+          </Flex>
+        </MenuButton>
       </Menu>
     </Flex>
   );
