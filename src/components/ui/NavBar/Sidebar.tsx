d9a08ad509744b2bfe645475d9c90a9e16b0a957
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-03 19:20:09 -0300

    refactor: Improve Sidebar component for clarity and readability following Clean Code principles

--- src/components/ui/NavBar/Sidebar.tsx
+++ src/components/ui/NavBar/Sidebar.tsx
@@ -3,22 +3,25 @@ import { useState } from "react";
 import DefaultNavigation from "./subcomponents/DefaultNav";
 import AccordionNav from "./subcomponents/AccordionNavigation";
 import UserInfos from "./subcomponents/UserInfos";
-import MenuButton from "./subcomponents/MenuIcon";
+import MenuButton from "./subcomponents/MenuButton";
 
 enum NavigationType {
   Default = "Default",
   Accordion = "Accordion",
 }
 
+const SMALL_SIZE = "small";
+const LARGE_SIZE = "large";
+
 interface ISidebarProps {
   type: NavigationType;
 }
 
 export default function Sidebar({ type }: ISidebarProps): JSX.Element {
-  const [navSize, setNavSize] = useState("large");
+  const [navSize, setNavSize] = useState(LARGE_SIZE);
 
   const toggleNavSize = (): void => {
-    setNavSize(navSize === "small" ? "large" : "small");
+    setNavSize(navSize === SMALL_SIZE ? LARGE_SIZE : SMALL_SIZE);
   };
 
   return (
@@ -27,13 +30,13 @@ export default function Sidebar({ type }: ISidebarProps): JSX.Element {
       position="sticky"
       left={5}
       h="95vh"
-      w={navSize === "small" ? "75px" : "200px"}
+      w={navSize === SMALL_SIZE ? "75px" : "200px"}
       marginTop="2.5vh"
       boxShadow="0 4px 12px 0 rgba(0, 0 , 0, 0.05)"
       flexDir="column"
       justifyContent="space-between"
     >
-      <Flex p="%5" flexDir="column" alignItems={navSize === "small" ? "center" : "flex-start"}>
+      <Flex p="%5" flexDir="column" alignItems={navSize === SMALL_SIZE ? "center" : "flex-start"}>
         <MenuButton onClick={toggleNavSize} />
         {type === NavigationType.Default && <DefaultNavigation navSize={navSize} />}
         {type === NavigationType.Accordion && <AccordionNav navSize={navSize} />}
