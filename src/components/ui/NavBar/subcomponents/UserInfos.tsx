0f7ec786a934bc143e062bc19edebefa73a27fab
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-03 19:28:06 -0300

    refactor: Enhance UserInfos component for clarity and consistency following Clean Code principles

--- src/components/ui/NavBar/subcomponents/UserInfos.tsx
+++ src/components/ui/NavBar/subcomponents/UserInfos.tsx
@@ -1,15 +1,21 @@
 import { Flex, Divider, Avatar, Heading } from "@chakra-ui/react";
-interface IUserInfos {
+
+interface IUserInfosProps {
   navSize: string;
 }
-export default function UserInfos({ navSize }: IUserInfos) {
+
+const SMALL_SIZE = "small";
+
+export default function UserInfos({ navSize }: IUserInfosProps): JSX.Element {
+  const isSmallSize = navSize === SMALL_SIZE;
+
   return (
-    <Flex p={"5%"} flexDir={"column"} w={"100%"} alignItems={"center"} mb={4}>
+    <Flex p="5%" flexDir="column" w="100%" alignItems="center" mb={4}>
       <Divider />
-      <Flex mt={4} justifyContent={"center"} flexDir={"column"} alignItems={"center"}>
-        <Avatar size={"md"} mb={2}></Avatar>
-        <Flex flexDir={"column"} ml={4}>
-          <Heading display={navSize == "small" ? "none" : "flex"} as={"h3"} size={"sm"}>
+      <Flex mt={4} justifyContent="center" flexDir="column" alignItems="center">
+        <Avatar size="md" mb={2}></Avatar>
+        <Flex flexDir="column" ml={4}>
+          <Heading display={isSmallSize ? "none" : "flex"} as="h3" size="sm">
             Nome do usuário
           </Heading>
         </Flex>
