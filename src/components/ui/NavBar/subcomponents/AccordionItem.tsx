3dee5c1fd56ae3190a1868d338a740d3703abf4e
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-03 19:23:34 -0300

    refactor: Improve code clarity and consistency in AccordionElement component following Clean Code principles

--- src/components/ui/NavBar/subcomponents/AccordionItem.tsx
+++ src/components/ui/NavBar/subcomponents/AccordionItem.tsx
@@ -1,30 +1,32 @@
 import { Accordion, AccordionButton, Icon, AccordionIcon, Box, AccordionItem, AccordionPanel } from "@chakra-ui/react";
 import NavItem from "./NavItem";
 
-interface IAccordionElement {
+interface IAccordionElementProps {
   navSize: string;
   icon?: React.ElementType;
   title: string;
   names: string[];
 }
 
-export default function AccordionElement({ navSize, icon, title, names }: IAccordionElement) {
+export default function AccordionElement({ navSize, icon, title, names }: IAccordionElementProps) {
+  const isSmallSize = navSize === "small";
+  const shouldRenderIcon = isSmallSize || (
+    <Box flex="1" alignContent="flex-start">
+      <Icon as={icon} /> {title}
+    </Box>
+  );
+
   return (
     <Accordion allowMultiple>
       <AccordionItem>
         <AccordionButton>
-          {navSize === "small" ? (
-            <Icon as={icon} />
-          ) : (
-            <Box flex="1" alignContent={"flex-start"}>
-              <Icon as={icon} /> {title}
-            </Box>
-          )}
+          {isSmallSize && <Icon as={icon} />}
+          {shouldRenderIcon}
           <AccordionIcon />
         </AccordionButton>
         <AccordionPanel>
-          {names.map((name, index) => (
-            <NavItem key={index} title={name} navSize={navSize} />
+          {names.map((name) => (
+            <NavItem key={name} title={name} navSize={navSize} />
           ))}
         </AccordionPanel>
       </AccordionItem>
