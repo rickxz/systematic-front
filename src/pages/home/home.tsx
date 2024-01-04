bff710621b0b16bb72ee49ccd83c48af502d0677
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-04 12:17:19 -0300

    feat: Add RevisionCard component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Card, Flex, Grid, GridItem, Heading, Icon, Text } from "@chakra-ui/react";
-import { IoNewspaperSharp } from "react-icons/io5";
-import { FaRegCircle } from "react-icons/fa6";
+import { Flex, Grid, GridItem, Heading } from "@chakra-ui/react";
+
 import Sidebar from "../../components/ui/NavBar/Sidebar";
+import RevisionCard from "./subcomponents/Card";
 
 export default function Home() {
   return (
@@ -12,50 +12,13 @@ export default function Home() {
           Minhas Revisões
         </Heading>
         <Flex mt={"2.5vh"}>
-          <Card
-            h={"100PX"}
-            w={"1100PX"}
-            boxShadow={"0 4px 12px 0 rgba(0, 0 , 0, 0.5)"}
-            display={"flex"}
-            flexDir="row"
-            borderRadius={"20px"}
-            ml={"5%"}
-            fontSize={"12px"}
-            justifySelf={"flex-start"}
-          >
-            <Icon
-              as={IoNewspaperSharp}
-              w={"60px"}
-              h={"60px"}
-              justifySelf={"center"}
-              alignSelf={"center"}
-              ml={"0.5rem"}
-            />
-            <Box
-              display={"flex"}
-              flexDir={"column"}
-              ml={"4.4rem"}
-              alignSelf={"center"}
-              justifySelf={"center"}
-              rowGap={"0.5em"}
-            >
-              <Text as={"h4"}>Titulo da revisão sistemática</Text>
-              <Text>Revisore: Revisor1....RevisorN</Text>
-            </Box>
-            <Box ml={"20%"}>
-              <Button ml={"100%"} mt={4} w={"106px"} h={"25px"}>
-                Ver Revisão
-              </Button>
-
-              <Box display={"flex"} flexDir={"row"} justifyContent={"space-between"} ml={"20%"} mt={"1.5rem"}>
-                <Icon as={FaRegCircle} size={"md"} color={"green"} mt={1.4} />
-                <Box display={"flex"} flexDir={"row"} ml={0.5}>
-                  <Text w={"200px"}>Data da ultima ediçao: dd/mm/yyyy</Text>
-                  <Text w={"200px"}>Data de criação: dd/mm/yyyy</Text>
-                </Box>
-              </Box>
-            </Box>
-          </Card>
+          <RevisionCard
+            title="Minha primeira revisao Sistemática"
+            RevisorNames={["Lucas Sigoli", "Eduardo Derisso", "Gabriel Gatti"]}
+            lastEdition="04/01.2024"
+            creation="01/01/2024"
+            isEdited
+          />
         </Flex>
       </GridItem>
     </Grid>
