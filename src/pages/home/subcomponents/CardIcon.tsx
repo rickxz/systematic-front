f67ab916936b2fbaa761b486f477bf52e5c86fa6
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-04 11:41:24 -0300

    feat: Create CardInfos component

--- src/pages/home/subcomponents/CardIcon.tsx
+++ src/pages/home/subcomponents/CardIcon.tsx
@@ -2,5 +2,5 @@ import { Icon } from "@chakra-ui/react";
 import { IoNewspaperSharp } from "react-icons/io5";
 
 export default function CardIcon() {
-  <Icon as={IoNewspaperSharp} w={"60px"} h={"60px"} justifySelf={"center"} alignSelf={"center"} ml={"0.5rem"} />;
+  return <Icon as={IoNewspaperSharp} w={"60px"} h={"60px"} justifySelf={"center"} alignSelf={"center"} ml={"0.5rem"} />;
 }
---
+++ src/pages/home/subcomponents/CardInfos.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@chakra-ui/react";
+
+interface ICardInfosProps {
+  title: string;
+  RevisorNames: string[];
+}
+export default function CardInfos({ title, RevisorNames }: ICardInfosProps) {
+  return (
+    <Box display={"flex"} flexDir={"column"} ml={"4.4rem"} alignSelf={"center"} justifySelf={"center"} rowGap={"0.5em"}>
+      <Text as={"h4"}>{title}</Text>
+      <Text>Revisores: {RevisorNames} </Text>
+    </Box>
+  );
+}
