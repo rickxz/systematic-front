c78034586e25ea9812bae864ecf6b222b2024a3e
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-04 11:36:36 -0300

    feat: Create CardIcon component

---
+++ src/pages/home/subcomponents/CardIcon.tsx
@@ -0,0 +1,6 @@
+import { Icon } from "@chakra-ui/react";
+import { IoNewspaperSharp } from "react-icons/io5";
+
+export default function CardIcon() {
+  <Icon as={IoNewspaperSharp} w={"60px"} h={"60px"} justifySelf={"center"} alignSelf={"center"} ml={"0.5rem"} />;
+}
