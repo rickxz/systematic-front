c65b987bd2b052a011ceeb01fbae40a191d9860e
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-02-06 14:31:55 -0300

    feat: add general info checkboxes

--- src/pages/NovaRevisao/finalization/Finalization.tsx
+++ src/pages/NovaRevisao/finalization/Finalization.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import EventButton from "../../../components/Buttons/EventButton";
+import NavButton from "../../../components/Buttons/NavButton";
+import InputText from "../../../components/Inputs/InputText";
+import SelectInput from "../../../components/Inputs/SelectInput";
 import DynamicTable from "../../../components/Tables/DynamicTable";
+import Header from "../../../components/ui/Header/Header";
 import Sidebar from "../../../components/ui/NavBar/Sidebar";
 
 import {Box, Flex} from "@chakra-ui/react";
+import CheckboxInput from "../../../components/Inputs/Checkbox";
 
 export default function Finalization() {
 
@@ -41,12 +48,57 @@ export default function Finalization() {
       100,
     ],
   ];
+
   return (
     <Flex>
       <Sidebar type="accordion"/>
 
       <Flex flexDirection="column" p="2%">
+        
+        <Box display={"flex"} flexDir={"row"} columnGap={5}>
+          <CheckboxInput
+            label="General Information: "
+            name={[
+              "ID PAPER",
+              "TITLE",
+              "AUTHOR",
+              "STATUS/SELECTION",
+              "STATUS/EXTRACTION",
+              "READING PRIORITY",
+              "SCORE",
+            ]}
+            value={["idPaper", "title", "author", "statusSelection", "statusExtraction", "readingPriority", "score"]}
+          />
+          <EventButton
+            mt={8}
+            text="Sort by Score"
+            event={() => {
+              console.log("sorted");
+            }}
+          />
+        </Box>
+
+        <Header text="Review Finalization" />
+
         <DynamicTable headerData={headerData} bodyData={bodyData} />
+
+        <Box
+          display={"flex"}
+          flexDir={"row"}
+          alignItems={"center"}
+          columnGap={5}
+          alignSelf={"baseline"}
+        >
+          <NavButton text={"Back"} path={"/newRevision/identification"} />
+          <EventButton
+            mt={2}
+            event={function (): void {
+              console.log("Finished the Review!");
+            }}
+            text={"Finish Review"}
+          />
+        </Box>
+        
       </Flex>
     </Flex>
   )
