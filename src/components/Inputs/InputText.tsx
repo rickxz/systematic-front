c041dfe7e9399e84fe0f754178ea4d318b35f182
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-02-15 11:41:43 -0300

    style: Correct position to title field of new revision component

--- src/components/Inputs/InputText.tsx
+++ src/components/Inputs/InputText.tsx
@@ -12,10 +12,10 @@ interface ITextFieldProps {
 export default function InputText({ label, placeholder, type, nome }: ITextFieldProps) {
   const isSearchField = type === "search";
   return (
-    <FormControl display={"flex"} flexDir={"column"} mt={isSearchField ? "" : 10} ml={isSearchField ? "" : "5em"}>
+    <FormControl display={"flex"} flexDir={"column"} mt={isSearchField ? "" : 10} maxW={"60vw"}>
       <FormLabel>{label}</FormLabel>
-      <FormControl display={"flex"} flexDir={"row"} gap={".3rem"} alignItems={"center"}>
-        <Input type={type} name={nome} placeholder={placeholder} w={isSearchField ? "250px" : "93.5%"} />
+      <FormControl display={"flex"} flexDir={"row"} gap={".3rem"}>
+        <Input type={type} name={nome} placeholder={placeholder} w={isSearchField ? "250px" : "100%"} />
         {isSearchField && <SearchButton />}
       </FormControl>
     </FormControl>
