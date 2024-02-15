0a0a97ecbb7f456bd39b44f8ea022110bd32069a
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-02-15 17:26:55 -0300

    refactor: Implements new components's version to finalization page

--- src/pages/NovaRevisao/finalization/Finalization.tsx
+++ src/pages/NovaRevisao/finalization/Finalization.tsx
@@ -1,124 +1,68 @@
-import { useState } from "react";
-import EventButton from "../../../components/Buttons/EventButton";
-import InputText from "../../../components/Inputs/InputText";
-import SelectInput from "../../../components/Inputs/SelectInput";
-import DynamicTable from "../../../components/Tables/DynamicTable";
+import { Box, Flex, Textarea } from "@chakra-ui/react";
+import useInputState from "../../../hooks/useInputState";
+import GridLayout from "../../../components/ui/Grid/Grid";
 import Header from "../../../components/ui/Header/Header";
-import Sidebar from "../../../components/ui/NavBar/Sidebar";
-
-import {Box, Flex, Grid, GridItem, Textarea} from "@chakra-ui/react";
+import InputText from "../../../components/Inputs/InputText";
 import CheckboxInput from "../../../components/Inputs/Checkbox";
+import SelectInput from "../../../components/Inputs/SelectInput";
+import EventButton from "../../../components/Buttons/EventButton";
+import DynamicTable from "../../../components/Tables/DynamicTable";
+import useFetchTableData from "../../../hooks/fetch/useFetchTableData";
 
 export default function Finalization() {
-
-  const headerData = ["IDSS", "ID Paper", "Title", "Author", "Year", "S/S", "S/E", "RP", "Score"];
-  const bodyData = [
-    [
-      "01",
-      "00001",
-      "Lorem ipsum dolor sit amet consectur",
-      "Jão da Silva, Cleitin, et all",
-      "2002",
-      "undefined",
-      "undefined",
-      "001",
-      100,
-    ],
-    [
-      "01",
-      "00001",
-      "Lorem ipsum dolor sit amet consectur",
-      "Jão da Silva, Cleitin, et all",
-      "2002",
-      "undefined",
-      "undefined",
-      "001",
-      100,
-    ],
-    [
-      "01",
-      "00001",
-      "Lorem ipsum dolor sit amet consectur",
-      "Jão da Silva, Cleitin, et all",
-      "2002",
-      "undefined",
-      "undefined",
-      "001",
-      100,
-    ],
-  ];
-
-  const handleSelectChange = (value: string) => {
-    setSelectedValue(value);
-  };
-
-  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+  const { headerData, bodyData } = useFetchTableData("/data/tableData.json");
+  const { value: selectedValue, handleChange: handleSelectChange } = useInputState<string | null>(null);
+  const { value: checkedValues, handleChange: handleCheckboxChange } = useInputState<string[]>([]);
 
   return (
-    <Grid templateColumns={"1fr 1fr 1fr"}>
-      <Sidebar type="Default"/>
-
-      <GridItem textAlign={"center"} justifySelf={"center"} w={"82vw"} marginLeft={5}>
-
-        <Header text="Review Finalization" />
-
-        <Flex flexDirection="column" gap={5}>
-          <Box mt={10} w={"80%"} display={"flex"} flexWrap={"wrap"} flexDir={"column"} rowGap={5}>
-            <Box display={"flex"} flexDir={"row"} w={"60%"} alignContent={"center"} columnGap={5}>
-              <InputText type="search" label="Search : " placeholder="Insert article's name" nome="search" />
-              <SelectInput
-                label="Classification:"
-                names={["", "Accepted", "Duplicated", "Rejected", "Unclassified"]}
-                values={["", "Accepted", "Duplicated", "Rejected", "Unclassified"]}
-                onSelect={handleSelectChange}
-                selectedValue={selectedValue}
-              />
-            </Box>
-          </Box>
-
-          <Box display={"flex"} flexDir={"row"} columnGap={5}>
-            <CheckboxInput
-              label="General Information: "
-              name={[
-                "ID PAPER",
-                "TITLE",
-                "AUTHOR",
-                "STATUS/SELECTION",
-                "STATUS/EXTRACTION",
-                "READING PRIORITY",
-                "SCORE",
-              ]}
-              value={["idPaper", "title", "author", "statusSelection", "statusExtraction", "readingPriority", "score"]}
-            />
-
-          <EventButton
-            mt={8}
-            text="Sort by Score"
-            event={() => {
-              console.log("sorted");
-            }}
+    <GridLayout navigationType="Accordion">
+      <Header text="Review Finalization" />
+      <Flex flexDirection="column" gap={5}>
+        <Box mt={10} w={"80%"} display={"flex"} flexWrap={"wrap"} flexDir={"column"} rowGap={5}>
+          <Box display={"flex"} flexDir={"row"} w={"60%"} alignContent={"center"} columnGap={5}>
+            <InputText type="search" label="Search : " placeholder="Insert article's name" nome="search" />
+            <SelectInput
+              label="Classification:"
+              names={["", "Accepted", "Duplicated", "Rejected", "Unclassified"]}
+              values={["", "Accepted", "Duplicated", "Rejected", "Unclassified"]}
+              onSelect={handleSelectChange}
+              selectedValue={selectedValue}
             />
           </Box>
-        </Flex>
-
-        <Box display="flex" flexDirection="column" gap={7}>
-          <DynamicTable headerData={headerData} bodyData={bodyData} />
-          <Textarea bg="gray.100" minHeight="20vh" placeholder="Write stuff here..."></Textarea>
-        
-          <Flex justifyContent="flex-end">
-            <EventButton
-              mt={2}
-              event={function (): void {
-                console.log("Export the Review!");
-              }}
-              text={"Export"}
-            />
-          </Flex>
         </Box>
-        
-        
 
-      </GridItem>
-    </Grid>
-  )
+        <Box display={"flex"} flexDir={"row"} columnGap={5}>
+          <CheckboxInput
+            label="General Information: "
+            name={headerData}
+            handleCheckboxChange={(selectedItems) => handleCheckboxChange(selectedItems)}
+            checkedByDefault={[
+              "idss",
+              "id paper",
+              "title",
+              "author",
+              "year",
+              "status/selection",
+              "status/extraction",
+              "reading priority",
+              "score",
+            ]}
+          />
+        </Box>
+      </Flex>
+      <Box display="flex" flexDirection="column" gap={7}>
+        <DynamicTable headerData={headerData} bodyData={bodyData} filteredColumns={checkedValues} />
+        <Textarea bg="gray.100" minHeight="20vh" placeholder="Write stuff here..."></Textarea>
+        <Flex justifyContent="flex-end">
+          <EventButton
+            mt={2}
+            event={function (): void {
+              console.log("Export the Review!");
+            }}
+            text={"Export"}
+          />
+        </Flex>
+      </Box>
+    </GridLayout>
+  );
 }
