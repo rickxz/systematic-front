38926bc03035df2305de67b185e4fde8a5bef035
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-02-18 20:08:24 -0300

    style: Correct styles of navButton and EventButton of New Revision page

--- src/pages/NovaRevisao/novaRevisao.tsx
+++ src/pages/NovaRevisao/novaRevisao.tsx
@@ -1,4 +1,4 @@
-import { FormControl } from "@chakra-ui/react";
+import { Box, FormControl } from "@chakra-ui/react";
 import GridLayout from "../../components/ui/Grid/Grid";
 import Header from "../../components/ui/Header/Header";
 import InputText from "../../components/Inputs/InputText";
@@ -12,8 +12,10 @@ export default function NovaRevisao() {
       <FormControl mt={"20px"} display={"flex"} gap={5} flexDir={"column"} w={"80%"} alignItems={"center"} ml={"10em"}>
         <InputText label="Title:" placeholder="Enter review title" type="text" nome="text" />
         <InputTextArea label="Description:" placeholder="Enter review description"></InputTextArea>
-        <ResearcherFilter></ResearcherFilter>
-        <NavButton path={"/newRevision/protocol"} text="Create new Review" />
+        <ResearcherFilter />
+        <Box ml={"80%"}>
+          <NavButton path={"/newRevision/protocol"} text="Create new Review" />
+        </Box>
       </FormControl>
     </GridLayout>
   );
