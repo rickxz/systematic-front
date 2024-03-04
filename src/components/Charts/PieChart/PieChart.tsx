4f02f835bebf8c80f612c61cf05bedd91621c348
gabie762 <gabihenriques821@gmail.com>
2024-03-04 18:36:48 -0300

    useFEtchData added to PieChart.tsx

--- src/components/Charts/PieChart/PieChart.tsx
+++ src/components/Charts/PieChart/PieChart.tsx
@@ -1,45 +1,42 @@
 import {
-    Chart as ChartJs,
-    ArcElement,
-    Tooltip,
-    Legend,
-    defaults,
-  } from "chart.js";
+  ArcElement,
+  Chart as ChartJs,
+  Legend,
+  Tooltip,
+  defaults,
+} from "chart.js";
 
-ChartJs.register(
-    ArcElement,
-    Tooltip,
-    Legend
-)
-  
-  import { Pie } from "react-chartjs-2";
-  import PieChartData from "../../../data/pieChartTest.json";
+ChartJs.register(ArcElement, Tooltip, Legend);
 
-  defaults.maintainAspectRatio = false;
-  defaults.responsive = true;
-  
-  
-  function PieChart() {
-    const data = {
-      labels: PieChartData.map((data) => data.label),
-      datasets: [
-        {
-          label: "Source",
-          data: PieChartData.map((data) => data.value),
-          backgroundColor: ["purple", "blue", "green", "lightblue"],
-        },
-      ],
-    };
-  
-    const options={
-            plugins: {
-                title: {
-                    text: "Source",
-                },
-            },
-        };
-  
-    return <Pie data={data} options={options} />;
-  }
-  
-  export default PieChart;
+import { Pie } from "react-chartjs-2";
+import PieChartData from "../../../data/pieChartTest.json";
+import useFetchDataBases from "../../../hooks/fetch/useFetchDataBases";
+
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+
+function PieChart() {
+  const database = useFetchDataBases("../../../data/pieChartTest.json");
+  const data = {
+    labels: PieChartData.map((data) => data.label),
+    datasets: [
+      {
+        label: "Source",
+        data: PieChartData.map((data) => data.value),
+        backgroundColor: ["purple", "blue", "green", "lightblue"],
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      title: {
+        text: "Source",
+      },
+    },
+  };
+
+  return <Pie data={data} options={options} />;
+}
+
+export default PieChart;
