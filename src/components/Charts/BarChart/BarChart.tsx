4fb37a0d938f1bf6a5953261101f574337efb10e
gabie762 <gabihenriques821@gmail.com>
2024-03-04 21:43:47 -0300

    Bar Chart fetch implemented

--- src/components/Charts/BarChart/BarChart.tsx
+++ src/components/Charts/BarChart/BarChart.tsx
@@ -1,32 +1,44 @@
 import {
-    BarElement,
-    CategoryScale,
-    Chart as ChartJs,
-    Legend,
-    LinearScale,
-    Tooltip,
-  } from "chart.js";
-  
-  import { Bar } from "react-chartjs-2";
-  import barChartData from "../../../data/barChartTest.json";
-  
-  ChartJs.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
-  
-  function BarChart() {
-    const data = {
-      labels: barChartData.map((data) => data.label),
-      datasets: [
-        {
-          label: "Extraction",
-          data: barChartData.map((data) => data.value),
-          backgroundColor: ["red", "blue", "green", "yellow"],
-        },
-      ],
-    };
-  
-    const options = {};
-  
-    return <Bar data={data} options={options} />;
-  }
-  
-  export default BarChart;
+  BarElement,
+  CategoryScale,
+  Chart as ChartJs,
+  Legend,
+  LinearScale,
+  Tooltip,
+  defaults,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+import useFetchGraphicsData from "../../../hooks/fetch/useFetchGraphicsData";
+
+ChartJs.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
+interface iGraphicsData {
+  label: string;
+  value: number;
+}
+
+function BarChart() {
+  const barChartData: iGraphicsData[] = useFetchGraphicsData(
+    "/data/barChartTest.json"
+  );
+  const data = {
+    labels: barChartData.map((data) => data.label),
+    datasets: [
+      {
+        label: "Extraction",
+        data: barChartData.map((data) => data.value),
+        backgroundColor: ["red", "blue", "green", "yellow"],
+      },
+    ],
+  };
+
+  const options = {};
+
+  defaults.maintainAspectRatio = false;
+  defaults.responsive = true;
+
+  return <Bar data={data} options={options} />;
+}
+
+export default BarChart;
