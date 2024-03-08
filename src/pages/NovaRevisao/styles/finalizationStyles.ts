00e3f7d9279166adf7e2b2deb5c29d5cfbd4afb2
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-03-08 18:05:18 -0300

    feat: Add finalizationStyles file

---
+++ src/pages/NovaRevisao/styles/finalizationStyles.ts
@@ -0,0 +1,35 @@
+export const conteiner = {
+  mt: 10,
+  w: "80%",
+  display: "flex",
+  flexWrap: "wrap",
+  flexDir: "column",
+  rowGap: 5,
+};
+export const inputcontrol = {
+  display: "flex",
+  flexDir: "row",
+  w: "60%",
+  alignContent: "center",
+  columnGap: 5,
+};
+export const ckControl = {
+  display: "flex",
+  flexDir: "row",
+  columnGap: 5,
+};
+
+export const tableConteiner = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 7,
+};
+
+export const textArea = {
+  bg: "gray.100",
+  minHeight: "20vh",
+};
+export const flexconteiner = {
+  flexDirection: "column",
+  gap: 5,
+};
