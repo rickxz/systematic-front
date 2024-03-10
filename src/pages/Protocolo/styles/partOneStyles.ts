ac70adcfd6f03f72dd39ea674e3cc4c012f6f7fa
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-03-10 16:28:07 -0300

    refactor: Separete styles on protocol page

---
+++ src/pages/Protocolo/styles/partOneStyles.ts
@@ -0,0 +1,13 @@
+export const formControl = {
+  display: "flex",
+  flexDir: "column",
+  gap: 20,
+  mt: 10,
+  alignItems: "center",
+};
+
+export const btnBox = {
+  ml: "55%",
+  alignSelf: "flex-end",
+  mt: 10,
+};
