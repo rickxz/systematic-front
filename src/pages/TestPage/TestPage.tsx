ffdbb27e57c5b0ac86bf43727c4538bb68e824c2
Eduardo Derisso <dudu.derisso@gmail.com>
2024-03-14 09:48:23 -0300

    feat: adicionado componente de todos os icones do modal de processamento de um estudo

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -2,6 +2,14 @@ import { Flex } from "@chakra-ui/react";
 import DataExtractionFormItensList from "../../components/Modals/Data Extraciton Field Creation/FormModalItensList";
 import DataExtractionFormItensValueList from "../../components/Modals/Data Extraciton Field Creation/FormModalItensValueList";
 import DataExtractionFormNumberInterval from "../../components/Modals/Data Extraciton Field Creation/FormModalNumberInterval";
+import {DataExtractionFormIcon} from "../../../public/icons/StudyModal/DataExtractionFormIcon"
+import {QualityFormIcon} from "../../../public/icons/StudyModal/QualityFormIcon";
+import {ReferencesIcon} from "../../../public/icons/StudyModal/ReferencesIcon";
+import {SelectionDataIcon} from "../../../public/icons//StudyModal/SelectionDataIcon";
+import {SimilarStudiesIcon} from "../../../public/icons/StudyModal/SimilarStudiesIcon";
+import {StudyDataIcon} from "../../../public/icons/StudyModal/StudyDataIcon";
+
+
 
 export default function TestPage() {
   return (
@@ -13,7 +21,12 @@ export default function TestPage() {
     </Flex>
 
     <Flex p='50px'>
-      ola
+      <DataExtractionFormIcon />
+      <QualityFormIcon/>
+      <ReferencesIcon/>
+      <SelectionDataIcon/>
+      <SimilarStudiesIcon/>
+      <StudyDataIcon/>
     </Flex>
       
     </>
