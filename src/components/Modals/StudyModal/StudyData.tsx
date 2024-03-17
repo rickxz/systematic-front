47940ea11c2d653fe0a47f97e8ab2cca189d71c3
gabie762 <gabihenriques821@gmail.com>
2024-03-17 00:11:44 -0300

    margins added to study data fields

--- src/components/Modals/StudyModal/StudyData.tsx
+++ src/components/Modals/StudyModal/StudyData.tsx
@@ -8,7 +8,7 @@ interface IStudyDataFiel {
 export default function StudyDataFiel({ studyData }: IStudyDataFiel) {
   return (
     
-    <Flex bg="gray.300" w="450px" h="350px" direction={"column"} alignContent={"center"} overflowY={"scroll"} /*justifyContent={"center"}*/ p="3">
+    <Flex bg="gray.300" w="450px" h="350px" direction={"column"} alignContent={"center"} overflowY={"scroll"} p="3">
       <Center>
         <Stack direction={"row"} p="2">
           <Text fontSize={"lg"}>Study Data</Text>
@@ -19,12 +19,25 @@ export default function StudyDataFiel({ studyData }: IStudyDataFiel) {
       <Text align={"center"} p="1">Autor SOBRENOME, autor 2 SOBRENOME</Text>
       
       <Flex direction={"column"} textAlign={"justify"} p="1">
-        <Text>
+        <Text marginBottom={"7px"}>
           <Text as="b">Keywords. </Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+
+        <Text marginBottom={"7px"}>
+          <Text as="b">Journal. </Text> Lorem ipsum dolor sit amet
+        </Text>
+
+        <Text marginBottom={"7px"}>
+          <Text as="b">Year. </Text> XXXX
+        </Text>
+
+        <Text marginBottom={"7px"}>
+          <Text as="b">Type. </Text> Lorem ipsum dolor sit amet
         </Text>
       
         <Text>
-          <Text as="b">Abstract. </Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Text>
+          <Text as="b">Abstract. </Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </Text>
       </Flex>
       {/*studyData.map((cell, cellIndex) => (
         <Input placeholder="cell" key={cellIndex} bg="gray.700" color="black" />
