c7605558843159b3f14a2880c8513e3602ea099e
Eduardo Derisso <dudu.derisso@gmail.com>
2024-03-22 10:58:56 -0300

    feat: adicionado combox de checkboxs para criterios de inclusão e exclusão na página de seleção

--- src/pages/Execution/SubPages/Selection/subcomponents/ButtonsForSelection.tsx
+++ src/pages/Execution/SubPages/Selection/subcomponents/ButtonsForSelection.tsx
@@ -1,13 +1,56 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { boxconteiner, buttonconteiner, conteiner } from "../../../styles/BtnSelectionStyles";
+import ComboBox from "../../../../../components/Inputs/ComboBox";
+import useInputState from "../../../../../hooks/useInputState";
+import useFetchTableData from "../../../../../hooks/fetch/useFetchTableData";
 
 export default function ButtonsForSelection() {
+  // Critérios de Exclusão (Exclusion Criteria)
+const criteriosExclusao: string[] = [
+  "Tipo estudo inadequado",
+  "Idioma não compreendido",
+  "Data publicação fora período",
+  "Publicação não revisada",
+  "População não incluída",
+  "Intervenção diferente",
+  "Comparador inadequado",
+  "Desfechos não medidos",
+  "Baixa qualidade metodológica",
+  "Dados incompletos/indisponíveis",
+];
+
+// Critérios de Inclusão (Inclusion Criteria)
+const criteriosInclusao: string[] = [
+  "Tipo estudo adequado",
+  "Idioma compreendido",
+  "Data publicação dentro período",
+  "Publicação revisada",
+  "População incluída",
+  "Intervenção igual",
+  "Comparador adequado",
+  "Desfechos medidos",
+  "Alta qualidade metodológica",
+  "Dados completos/disponíveis",
+];
+  const { value: checkedValues, handleChange: handleCheckboxChange } = useInputState<string[]>([]);
+
+
   return (
     <>
       <Flex sx={conteiner}>
         <Flex sx={boxconteiner}>
-          <Box bg="blue.200">Inclusion Critiries</Box>
-          <Box bg="blue.200">Exclusion Critiries</Box>
+          <ComboBox
+              text="filter options"
+              options={criteriosExclusao}
+              handleCheckboxChange={handleCheckboxChange}
+              selectedItems={criteriosExclusao}
+            />
+            <ComboBox
+              text="filter options"
+              options={criteriosInclusao}
+              handleCheckboxChange={handleCheckboxChange}
+              selectedItems={criteriosInclusao}
+            />
         </Flex>
 
         <Flex sx={buttonconteiner}>
