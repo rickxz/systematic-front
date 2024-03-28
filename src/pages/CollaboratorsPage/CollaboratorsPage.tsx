b90cee63aa3b68e0e82e7db3c5ccfebd6e69413b
Eduardo Derisso <dudu.derisso@gmail.com>
2024-03-28 11:38:39 -0300

    fix: removendo importações desnecessários no arquivo CollaboratorsPage.tsx

--- src/pages/CollaboratorsPage/CollaboratorsPage.tsx
+++ src/pages/CollaboratorsPage/CollaboratorsPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
 import Header from "../Homepage/subcomponents/Header/Header";
 import Footer from "../Homepage/subcomponents/Footer/Footer";
 import CollaboratorCard from "./subcomponents/collaboratorCards";
