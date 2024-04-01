282b61c7ca74dcad29edd0474a9607500d13c862
gabie762 <gabihenriques821@gmail.com>
2024-04-01 17:01:35 -0300

    creta mockup data and fetch for study data

--- src/hooks/fetch/useFetchStudyData.tsx
+++ src/hooks/fetch/useFetchStudyData.tsx
@@ -1,13 +1,31 @@
 import { useState, useEffect } from "react";
 
-interface StudyData {
+interface IStudyData {
     title: string;
-    authors:string;
-
+    authors:string[];
+    
 }
 
 const useFetchStudyData = (url: string) => {
-
+    const[studyData, setStudyData] = useState<IStudyData[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url);
+            const data = await response.json();
+            if (data) {
+              setStudyData(data);
+            } else {
+              console.error("O arquivo JSON não possui a estrutura esperada.");
+            }
+          } catch (error) {
+            console.error("Erro ao buscar os dados:", error);
+          }
+        };
+    
+        fetchData();
+      }, [url]);
+      return studyData;
 }
 
 export default useFetchStudyData;
