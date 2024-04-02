8136cffb782f378b981aecc6bb7cd20c9d1844e9
gabie762 <gabihenriques821@gmail.com>
2024-04-02 08:44:31 -0300

    alter fetch study data psoition

--- src/hooks/fetch/useFetchStudyData.tsx
+++ src/hooks/fetch/useFetchStudyData.tsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 
 interface IStudyData {
     title: string;
+    authors: string[];
     year:string;
     venue: string;
-    authors: string[];
     abstract: string;
     keywords:string[];
     type:string;
