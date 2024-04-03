8c417ed023ca6c4d978acfdb882e1c3637013803
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-03 12:31:52 -0300

    fix: Fix StudySelectionArea issue

--- src/pages/Execution/SubPages/Selection/subcomponents/StudySelectionArea.tsx
+++ src/pages/Execution/SubPages/Selection/subcomponents/StudySelectionArea.tsx
@@ -1,14 +1,18 @@
 import { Box, Flex } from "@chakra-ui/react";
 import ButtonsForSelection from "./ButtonsForSelection";
 import StudyDataFiel from "../../../../../components/Modals/StudyModal/StudyData";
+import useFetchStudyData from "../../../../../hooks/fetch/useFetchStudyData";
 
 export default function StudySelectionArea() {
+  const studyData = useFetchStudyData("../../../../public/data/studyData.json");
+  const studyDataAvailable = studyData && studyData.length > 0;
+
   return (
     <>
       <Flex mt="10" direction="column" bg="gray.600" w="auto" p="5" alignItems={"center"}>
         <ButtonsForSelection />
         <Box w={"100%"} bg="gray.200">
-          <StudyDataFiel studyData={[]} type="Selection"/>
+          {studyDataAvailable && <StudyDataFiel studyData={[studyData[0]]} type="Selection" />}
         </Box>
       </Flex>
     </>
