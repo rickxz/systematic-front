9bada6f7eb002e6e71fe7e781d406a8ffc3ff9ea
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-08 12:06:00 -0300

    feat: Implements combobox filter to SearchSection page

--- src/pages/SearchSession/SearchSession.tsx
+++ src/pages/SearchSession/SearchSession.tsx
@@ -9,6 +9,8 @@ import useFetchTableData from "../../hooks/fetch/useFetchTableData";
 import SearchInformations from "./subcomponents/searchInformations";
 import { ckbox, conteiner, navbtnStyles } from "./styles/searchSessionStyles";
 import ComboBox from "../../components/Inputs/ComboBox";
+import { flex } from "../NovaRevisao/styles/finalizationStyles";
+import EventButton from "../../components/Buttons/EventButton";
 
 export default function SearchSession() {
   const { headerData, bodyData } = useFetchTableData("/data/tableData.json");
@@ -18,24 +20,34 @@ export default function SearchSession() {
       <Header text={"Database Name-Studies Identification"} />
       <Box sx={conteiner}>
         <SearchInformations />
-        <Upload />
+        <Box display={flex} flexDir={"column"}>
+          <Upload />
+          <Box mt={5} display={flex} flexDir={"row"} justifyContent={"space-evenly"}>
+            <ComboBox
+              options={headerData}
+              handleCheckboxChange={handleCheckboxChange}
+              selectedItems={[
+                "title",
+                "author",
+                "year",
+                "status/selection",
+                "status/extraction",
+                "reading priority",
+                "score",
+              ]}
+              text={"filter options"}
+            />
+            <EventButton
+              ml={4}
+              event={function (): void {
+                window.alert("Duplicated Pappers removed");
+              }}
+              text={"Remove Duplicated Pappers"}
+            />
+          </Box>
+        </Box>
       </Box>
-      <Box sx={ckbox}>
-        <ComboBox
-          options={headerData}
-          handleCheckboxChange={handleCheckboxChange}
-          selectedItems={[
-            "title",
-            "author",
-            "year",
-            "status/selection",
-            "status/extraction",
-            "reading priority",
-            "score",
-          ]}
-          text={"filter options"}
-        />
-      </Box>
+      <Box sx={ckbox}></Box>
       <DynamicTable headerData={headerData} bodyData={bodyData} filteredColumns={checkedValues} />
       <NavButton text={"Back"} path={"/newRevision/identification"} sx={navbtnStyles} />
     </GridLayout>
