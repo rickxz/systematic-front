cd96f9d87de1c7c2708e76841dfb4e651f3f0f3e
Vitor5bonelli <bonelli5.vitor@gmail.com>
2024-04-09 13:31:05 -0300

    style(revisionCardStyles): add responsive structure

--- src/pages/UserArea/styles/revisionCardstyles.ts
+++ src/pages/UserArea/styles/revisionCardstyles.ts
@@ -1,15 +1,14 @@
 export const Cardstyles = {
-  h: "100px",
-  w: "1001px",
+  h: {base:"16em", sm:"14em", md:"12em", lg:"10em", xl:"8em"},
+  w: {base:"100px", sm:"300px", md:"400px", lg:"650px", xl:"1020px"},
   boxShadow: "0 4px 12px 0 rgba(0, 0 , 0, 0.5)",
   display: "flex",
-  flexDir: "row",
+  flexDir: {base:"column", sm:"column", md:"column", lg:"row", xl:"row"},
   borderRadius: "20px",
-  ml: "5%",
   fontSize: "12px",
   justifySelf: "flex-start",
 };
 export const CardInfosConteiner = {
-  ml: "150px",
-  w: "fit-content",
+  ml: "100px",
+  w: {base:"100px", sm:"300px", md:"400px", lg:"650px", xl:"1020px"},
 };
