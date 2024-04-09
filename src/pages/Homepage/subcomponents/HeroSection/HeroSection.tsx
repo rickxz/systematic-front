652f7c57af74862bb905db05c3f219f6055c1fdc
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-09 14:20:53 -0300

    fix: alterado layout do componente de HeroSection

--- src/pages/Homepage/subcomponents/HeroSection/HeroSection.tsx
+++ src/pages/Homepage/subcomponents/HeroSection/HeroSection.tsx
@@ -1,31 +1,34 @@
+import { Flex, Image, Box, Heading, Text, Button } from "@chakra-ui/react";
+
 export default function HeroSection() {
     return (
         <Flex
-      direction={mainDirection}
+        bg=" black"
       alignItems="center"
       gap="50px"
       pt="30px"
       pb="30px"
-      pl="300px"
-      pr="300px"
-      background={styles.background}
-      id={id}
+      pl="200px"
+      pr="200px"
+      justifyContent={"center"}
     >
-      {" "}
-      <Box paddingTop={"4%"} h="75vh" display={"flex"} justifyContent={"center"} flexDir={mainDirection} gap={10}>
-        <Flex gap="25px">
-              <Image src={src} alt={alt} borderRadius="30px" />
-              <Flex direction="column" alignItems="center" gap="50px" order={textIndex}>
-                <Heading color={styles.textColor} alignSelf={"center"}>
-                  {header}
+        <Flex gap="5px">
+              <Flex direction="column" alignItems="center" gap="5px">
+                <Heading color="white" alignSelf={"center"} fontSize={"70"} pb="130px">
+                Pesquise mais, de qualquer lugar!
                 </Heading>
-                  <Text color={styles.textColor}>{bodyText}</Text>
-                  <Button w="40%" colorScheme={styles.buttonColor}>
-                    Saiba mais
+                  <Text color="white" fontSize={"16"}>{`Potencialize suas pesquisas! Experimente nossa 
+                  ferramenta gratuita para revisões sistemáticas - fácil de usar e ideal 
+                  para pesquisadores e estudantes.`}</Text>
+                  <Button w="40%" colorScheme="whiteAlpha">
+                    Cadastre-se já
                   </Button>
               </Flex>
+              <Image src="../../../../../public/photos/homepagePhotos/HeroSectionPic.png" 
+              alt={`Foto de diferentes dispositivos eletronicos conectados a ferramenta StArt 
+              - computador deskot, smart phone e tablet.`} 
+              borderRadius="30px" />
         </Flex>
-      </Box>
     </Flex>
     );
 }
