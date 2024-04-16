fbc0f3f86b6c7cbfa9f8126f19717006cffedba8
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-16 14:40:27 -0300

    feat: preenchendo props do Contact Card no componente ContactSextion

--- src/pages/Homepage/subcomponents/ContactSection/ContactSection.tsx
+++ src/pages/Homepage/subcomponents/ContactSection/ContactSection.tsx
@@ -6,11 +6,10 @@ export default function ContactSection() {
     return (
         <Flex justifyContent="center">
             <Flex wrap={"wrap"} w="500px" h="500px" gap="50px">
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-
+                <ContactCard icon={""} title={"Nosso escritório principal"} text={"Rua dos Bobos, nº 0"} />
+                <ContactCard icon={""} title={"Número de telefone"} text={"(16) 98765-4321"} />
+                <ContactCard icon={""} title={"Fax"} text={"(16) 98765-4321"} />
+                <ContactCard icon={""} title={"O email"} text={"nosso_email@gmail.com.br"} />
             </Flex>
             <EmailArea />
         </Flex>
