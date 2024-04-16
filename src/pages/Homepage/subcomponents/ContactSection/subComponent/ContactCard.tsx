3a06cc11624027717e8282928cf23f87907aaf02
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-16 14:35:17 -0300

    feat: personalizando conteudo do card de contato por meio de props

--- src/pages/Homepage/subcomponents/ContactSection/subComponent/ContactCard.tsx
+++ src/pages/Homepage/subcomponents/ContactSection/subComponent/ContactCard.tsx
@@ -1,7 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Icon, Text } from "@chakra-ui/react";
 
-export default function ContactCard() {
+interface ContactInterface{
+    icon: string,
+    title: string,
+    text: string
+}
+
+export default function ContactCard({icon, title, text}: ContactInterface) {
     return (
-        <Box w="200px" h="200px" bg="black">EMail</Box>
+        <Flex w="200px" h="200px" bg="gray" alignItems="center" direction={"column"} p="20px">
+            <Icon/>
+            <Text>{title}</Text>
+            <Text>{text}</Text>
+        </Flex>    
     );
 }
