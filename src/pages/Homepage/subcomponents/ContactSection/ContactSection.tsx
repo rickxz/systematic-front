1678c0e15dc96adc2f9c56fb1ac9c4f74e45d028
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-16 14:49:42 -0300

    feat: ajeitado tamanho do area de email

--- src/pages/Homepage/subcomponents/ContactSection/ContactSection.tsx
+++ src/pages/Homepage/subcomponents/ContactSection/ContactSection.tsx
@@ -5,7 +5,7 @@ import EmailArea from "./subComponent/EmailArea";
 export default function ContactSection() {
     return (
         <Flex justifyContent="center">
-            <Flex wrap={"wrap"} w="500px" h="500px" gap="50px">
+            <Flex wrap={"wrap"} w="500px" h="500px" gap="20px">
                 <ContactCard icon={""} title={"Nosso escritório principal"} text={"Rua dos Bobos, nº 0"} />
                 <ContactCard icon={""} title={"Número de telefone"} text={"(16) 98765-4321"} />
                 <ContactCard icon={""} title={"Fax"} text={"(16) 98765-4321"} />
--- src/pages/Homepage/subcomponents/ContactSection/subComponent/EmailArea.tsx
+++ src/pages/Homepage/subcomponents/ContactSection/subComponent/EmailArea.tsx
@@ -2,6 +2,6 @@ import { Box } from "@chakra-ui/react";
 
 export default function EmailArea() {
     return (
-        <Box h="500px" w="500px" bg="black">Area de email</Box>
+        <Box h="400px" w="400px" bg="black">Area de email</Box>
     );
 }
