824bd7dd8414f9b8447060253c8283fcb51c50ac
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-16 14:59:52 -0300

    feat: adicionado props e inpurs na area de email

--- src/pages/Homepage/subcomponents/ContactSection/subComponent/EmailArea.tsx
+++ src/pages/Homepage/subcomponents/ContactSection/subComponent/EmailArea.tsx
@@ -1,7 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Button, Flex, Heading, Input } from "@chakra-ui/react";
 
-export default function EmailArea() {
+interface EmailAreaInterface{
+    title: string,
+    buttonText: string
+}
+
+export default function EmailArea({title, buttonText}: EmailAreaInterface) {
     return (
-        <Box h="400px" w="400px" bg="black">Area de email</Box>
+        <Flex h="400px" w="400px" bg="gray">
+            <Heading fontSize="20">{title}</Heading>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Button>{buttonText}</Button>
+        </Flex>
     );
 }
