b47f2c9567a10b1d46662dbf14e6492f3cb0a66c
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-04-17 23:21:42 -0300

    feat(eventbutton): add variant switcher

--- src/components/Buttons/EventButton.tsx
+++ src/components/Buttons/EventButton.tsx
@@ -3,15 +3,41 @@ import { Button, ButtonProps } from "@chakra-ui/react";
 interface IEventButtonProps extends ButtonProps {
   event: () => void;
   text: string;
+  variant?: "default" | "dark";
 }
 
-export default function EventButton({ event, text, ...buttonProps }: IEventButtonProps) {
+const variants = {
+  default: {
+    bgColor: "#526D82",
+    color: "#E5EBEF",
+    hoverBgColor: "#C9D9E5",
+    hoverColor: "#526D82",
+  },
+  dark: {
+    bgColor: "#FDF0D5",
+    color: "#301E1A",
+    hoverBgColor: "#301E1A",
+    hoverColor: "#FDF0D5",
+  },
+}
+
+export default function EventButton({ event, text, variant="default", ...buttonProps }: IEventButtonProps) {
   const handleClick = () => {
     event();
   };
 
+  const { bgColor, color, hoverBgColor, hoverColor } = variants[variant];
+
   return (
-    <Button onClick={handleClick} {...buttonProps}>
+    <Button 
+    bgColor={bgColor} 
+    color={color}
+    _hover={{ 
+      bgColor: hoverBgColor,
+      color: hoverColor,
+     }}
+     
+     onClick={handleClick} {...buttonProps}>
       {text}
     </Button>
   );
