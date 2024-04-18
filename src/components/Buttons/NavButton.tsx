1c385e5919c8d4aa00832352fa4b6ef615f743db
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-04-17 21:45:40 -0300

    feat(navButton): create variant switcher for nav buttons

--- src/components/Buttons/NavButton.tsx
+++ src/components/Buttons/NavButton.tsx
@@ -4,17 +4,37 @@ import { Link } from "react-router-dom";
 interface iButtonProps extends ButtonProps {
   text: string;
   path: string;
+  variant?: "default" | "dark";
 }
 
-export default function NavButton({ text, path, ...buttonProps }: iButtonProps) {
+const variants = {
+  default: {
+    bgColor: "#526D82",
+    color: "#E5EBEF",
+    hoverBgColor: "#C9D9E5",
+    hoverColor: "#526D82",
+  },
+  dark: {
+    bgColor: "#FDF0D5",
+    color: "#301E1A",
+    hoverBgColor: "#301E1A",
+    hoverColor: "#FDF0D5",
+  },
+}
+
+export default function NavButton({ text, path, variant = "default", ...buttonProps }: iButtonProps) {
+  const { bgColor, color, hoverBgColor, hoverColor } = variants[variant];
   return (
     <Link to={path}>
-      <Button bgColor={"#526D82"} color={"#E5EBEF"} 
+
+      <Button bgColor={bgColor} color={color}
           _hover={{ 
-            bgColor:"#C9D9E5",
-            color:"#526D82",
+            bgColor: hoverBgColor,
+            color: hoverColor,
            }}
-           {...buttonProps}>{text}</Button>
+           {...buttonProps}>{text}
+      </Button>
+           
     </Link>
   );
 }
