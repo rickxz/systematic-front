2bf7396984eca6c0b6fc1178943d4edfa9d4e7c6
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-19 17:53:05 -0300

    feat: criado tipo de objeto estudo

--- public/interfaces/IStudy.tsx
+++ public/interfaces/IStudy.tsx
@@ -1,9 +1,29 @@
-export interface IStudy {
+import {StudyType} from "./../enums/studyType";
+import {CriteriaType} from "./../enums/CriteriaType";
+import {ReadingPriority} from "./../enums/readingPriority";
+import {SelectionStatus} from "./../enums/selectionStatus";
+import {ExtractionStatus} from "./../enums/extractionStatus";
+
+
+
+interface StudyReview {
+    studyId: number;
+    systematicStudyId: number;
+    studyType: StudyType;
     title: string;
-    authors: string[];
-    year: string;
+    year: number;
+    authors: string;
     venue: string;
     abstract: string;
+    doi?: string | null;
     keywords: string[];
-    type: string;
+    searchSources: string[];
+    references: string[];
+    criteria: CriteriaType;
+    formAnswers: any[]; // Defina o tipo correto para a resposta do formulário
+    robAnswers: any[]; // Defina o tipo correto para a resposta do ROB
+    comments: string;
+    readingPriority: ReadingPriority;
+    selectionStatus: SelectionStatus;
+    extractionStatus: ExtractionStatus;
 }
