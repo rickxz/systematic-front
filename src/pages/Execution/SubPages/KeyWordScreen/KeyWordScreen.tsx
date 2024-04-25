46f160317f249b8d7a0984ce9ee9d4e13677f907
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-04-25 05:27:49 -0300

    refactor(keywordscreen): replace gridlayout to flexlayout

--- src/pages/Execution/SubPages/KeyWordScreen/KeyWordScreen.tsx
+++ src/pages/Execution/SubPages/KeyWordScreen/KeyWordScreen.tsx
@@ -1,4 +1,4 @@
-import GridLayout from "../../../../components/ui/Grid/Grid";
+import FlexLayout from "../../../../components/ui/Flex/Flex";
 import Header from "../../../../components/ui/Header/Header";
 import DynamicTable from "../../../../components/Tables/DynamicTable";
 import useFetchTableData from "../../../../hooks/fetch/useFetchTableData";
@@ -6,9 +6,9 @@ import useFetchTableData from "../../../../hooks/fetch/useFetchTableData";
 export default function KeyWordScreen() {
   const { headerData, bodyData } = useFetchTableData("/data/keywordData.json");
   return (
-    <GridLayout defaultOpen={1} navigationType="Accordion">
+    <FlexLayout defaultOpen={1} navigationType="Accordion">
       <Header text="Keyword Screen" />
       <DynamicTable headerData={headerData} bodyData={bodyData} type="keyword" filteredColumns={[]} />
-    </GridLayout>
+    </FlexLayout>
   );
 }
