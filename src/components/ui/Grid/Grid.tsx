895b52128dc94b28ca57a3432e67b6cf417e7bcc
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-04-25 03:09:38 -0300

    feat: create flex component

--- src/components/ui/Grid/Grid.tsx
+++ src/components/ui/Grid/Grid.tsx
@@ -14,6 +14,7 @@ export default function GridLayout({ navigationType, children, defaultOpen }: iG
       <GridItem>
         <Sidebar type={navigationType} defaultOpen={defaultOpen} />
       </GridItem>
+      
       <GridItem sx={gridItem}>{children}</GridItem>
     </Grid>
   );
