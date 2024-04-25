f96ecca1d940b2e4870491cdc1f151acd61ed20a
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-04-25 06:09:22 -0300

    style(protocol): fix protocol layout

--- src/pages/Protocolo/Protocol.tsx
+++ src/pages/Protocolo/Protocol.tsx
@@ -1,5 +1,5 @@
-import { Progress } from "@chakra-ui/react";
-import { Box, FormControl } from "@chakra-ui/react";
+import { Progress, Spacer } from "@chakra-ui/react";
+import { Box, FormControl, Flex } from "@chakra-ui/react";
 import FlexLayout from "../../components/ui/Flex/Flex";
 import Header from "../../components/ui/Header/Header";
 import NavButton from "../../components/Buttons/NavButton";
@@ -9,15 +9,24 @@ import TextAreaInput from "../../components/Inputs/InputTextArea";
 export default function Protocol() {
   return (
     <FlexLayout defaultOpen={0} navigationType="Accordion">
+
       <Header text="Protocol" />
+
       <Progress value={0} />
-      <FormControl sx={formControl}>
-        <TextAreaInput label="Objectives:" placeholder="What are your goals?" />
-        <TextAreaInput label="Main question:" placeholder="The reason behind your research..." />
-      </FormControl>
-      <Box sx={btnBox}>
-        <NavButton text="Next" path="/newRevision/protocolpartTwo" w={"30%"} />
-      </Box>
+
+      <Flex justify={"center"} direction={"column"}>
+
+        <FormControl sx={formControl}>
+          <TextAreaInput label="Objectives:" placeholder="What are your goals?" />
+          <TextAreaInput label="Main question:" placeholder="The reason behind your research..." />
+        </FormControl>
+
+        <Box sx={btnBox}>
+          <NavButton text="Next" path="/newRevision/protocolpartTwo" w={"30%"} />
+        </Box>
+
+      </Flex>
+
     </FlexLayout>
   );
 }
