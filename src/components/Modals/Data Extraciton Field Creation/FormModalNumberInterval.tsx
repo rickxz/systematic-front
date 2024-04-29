dfc2e4bd437a8367dc3812f8e2129dd2a9f7bd69
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-29 17:50:03 -0300

    fix: Correct FormModalNumberIntervals and ReferenceCard atributes

--- src/components/Modals/Data Extraciton Field Creation/FormModalNumberInterval.tsx
+++ src/components/Modals/Data Extraciton Field Creation/FormModalNumberInterval.tsx
@@ -1,47 +1,53 @@
 import {
-    Modal,
-    ModalOverlay,ModalContent,
-    ModalHeader,ModalFooter,
-    ModalBody, ModalCloseButton,
-    Button, Flex, useDisclosure, Input
-  } from '@chakra-ui/react';
-import InputText from '../../Inputs/InputText';
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Flex,
+  useDisclosure,
+  Input,
+} from "@chakra-ui/react";
+import InputText from "../../Inputs/InputText";
 
 export default function DataExtractionFormNumberInterval() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Button onClick={onOpen}>Open Modal</Button>
 
-      <Modal isOpen={isOpen} onClose={onClose} size={'sm'}>
+      <Modal isOpen={isOpen} onClose={onClose} size={"sm"}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Data Extraction Field Creation</ModalHeader>
           <ModalCloseButton />
           <ModalBody bg="gray.300">
-              <Flex flexDirection="column" gap="5" p="20">
-                      <InputText label='Min Number:' placeholder={'digite o número...'} type={'number'} nome={''}/>
-                      <InputText label='Max Number:' placeholder={'digite o número...'} type={'number'} nome={''}/>
-                      <InputText label='Increment:' placeholder={'digite o número...'} type={'number'} nome={''}/>
-                  <Flex gap="5" justifyContent="flex-end">
-                      <Button>Ok</Button>
-                      <Button>Cancel</Button>
-                  </Flex>
-                  <Flex>
-                      <Input type="number"></Input>
-                  </Flex>
+            <Flex flexDirection="column" gap="5" p="20">
+              <InputText placeholder={"digite o número..."} type={"number"} nome={""} />
+              <InputText placeholder={"digite o número..."} type={"number"} nome={""} />
+              <InputText placeholder={"digite o número..."} type={"number"} nome={""} />
+              <Flex gap="5" justifyContent="flex-end">
+                <Button>Ok</Button>
+                <Button>Cancel</Button>
               </Flex>
+              <Flex>
+                <Input type="number"></Input>
+              </Flex>
+            </Flex>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button variant='ghost'>Secondary Action</Button>
+            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
