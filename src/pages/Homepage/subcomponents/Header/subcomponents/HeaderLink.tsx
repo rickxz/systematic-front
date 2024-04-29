ce9c17407edd64427ad5de4e645ab06525f0361d
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-29 18:16:50 -0300

    feat: Implements HashRouter to solve router issues

--- src/pages/Homepage/subcomponents/Header/subcomponents/HeaderLink.tsx
+++ src/pages/Homepage/subcomponents/Header/subcomponents/HeaderLink.tsx
@@ -1,38 +1,39 @@
 import { useState } from "react";
 import { DarkText, LightText } from "./styles/LinkTheme";
 import { DarkBackground, LightBackground } from "./styles/LinkTheme";
-
 import { Box, Text } from "@chakra-ui/react";
 
 enum LinkTypeEnum {
   GoToOtherPage = "GoToOtherPage",
-  StayInSamePage = "StayInSamePage"
+  StayInSamePage = "StayInSamePage",
 }
-
 
 interface IHeaderLink {
   text: string;
   id: string;
-  type: LinkTypeEnum
+  type: LinkTypeEnum;
 }
 
 export default function HeaderLink({ text, id, type }: IHeaderLink) {
-  const [onHover, SetOnHover] = useState(false);
-  console.log(id);
+  const [onHover, setOnHover] = useState(false);
+
+  const handleAnchorClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
 
   return (
-    <>
-      <a href={type==LinkTypeEnum.StayInSamePage ? `#${id}` : id }>
-        <Box sx={onHover ? DarkBackground : LightBackground}>
-          <Text
-            onMouseEnter={() => SetOnHover(true)}
-            onMouseLeave={() => SetOnHover(false)}
-            sx={onHover ? DarkText : LightText}
-          >
-            {text}
-          </Text>
-        </Box>
-      </a>
-    </>
+    <a href={`#${id}`} onClick={type === LinkTypeEnum.StayInSamePage ? handleAnchorClick : undefined}>
+      <Box
+        sx={onHover ? DarkBackground : LightBackground}
+        onMouseEnter={() => setOnHover(true)}
+        onMouseLeave={() => setOnHover(false)}
+      >
+        <Text sx={onHover ? DarkText : LightText}>{text}</Text>
+      </Box>
+    </a>
   );
 }
