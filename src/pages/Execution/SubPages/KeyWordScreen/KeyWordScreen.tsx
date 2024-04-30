fb30c7a0182f7863ff003b497ef969a917d9d157
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-30 15:57:58 -0300

    feat: Implement scroll conteiner to keyword  page

--- src/pages/Execution/SubPages/KeyWordScreen/KeyWordScreen.tsx
+++ src/pages/Execution/SubPages/KeyWordScreen/KeyWordScreen.tsx
@@ -1,19 +1,18 @@
-import FlexLayout from "../../../../components/ui/Flex/Flex";
 import Header from "../../../../components/ui/Header/Header";
 import DynamicTable from "../../../../components/Tables/DynamicTable";
 import useFetchTableData from "../../../../hooks/fetch/useFetchTableData";
 import { Flex } from "@chakra-ui/react";
+import GridLayout from "../../../../components/ui/Grid/Grid";
 
 export default function KeyWordScreen() {
   const { headerData, bodyData } = useFetchTableData("/data/keywordData.json");
   return (
-    <FlexLayout defaultOpen={1} navigationType="Accordion">
-        <Header text="Keyword Screen" />
+    <GridLayout defaultOpen={1} navigationType="Accordion">
+      <Header text="Keyword Screen" />
 
       <Flex marginLeft={"4em"} marginRight={"4em"}>
         <DynamicTable headerData={headerData} bodyData={bodyData} type="keyword" filteredColumns={[]} />
       </Flex>
-
-    </FlexLayout>
+    </GridLayout>
   );
 }
