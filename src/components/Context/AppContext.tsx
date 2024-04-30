adcf4debea3a3b2b058a718114652e03c00599f4
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-30 09:56:14 -0300

    feat: add studys variables in the global context to enable the objet transmition for Modals ou StudySelectionArea easily

--- src/components/Context/AppContext.tsx
+++ src/components/Context/AppContext.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode, createContext , useState } from 'react';
+import { StudyInterface } from "../../../public/interfaces/IStudy";
+import showFirstPossibleStudy from '../../hooks/seachAppropriateStudy/showFirstPossibleStudy';
+import { ExcutionFaseEnum } from '../../../public/enums/ExcutionFaseEnum';
 
 interface AppContextType {
     button: string;
@@ -6,7 +9,12 @@ interface AppContextType {
     item: string;
     setItem: React.Dispatch<React.SetStateAction<string>>;
     renderForm: string;
-    SetRenderForm: React.Dispatch<React.SetStateAction<string>>;
+    setRenderForm: React.Dispatch<React.SetStateAction<string>>;
+
+    selectionStudy: StudyInterface;
+    setSelectionStudy: React.Dispatch<React.SetStateAction<StudyInterface>>;
+    extractionStudy: StudyInterface;
+    setExtractionStudy: React.Dispatch<React.SetStateAction<StudyInterface>>;
 }
 
 const AppContext = createContext<AppContextType | undefined >(undefined);
@@ -18,10 +26,12 @@ interface AppProviderProps {
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
     const [button, setButton] = useState<string>("");
     const [item, setItem] = useState<string>("");
-    const [renderForm, SetRenderForm] = useState<string>("Login");
+    const [renderForm, setRenderForm] = useState<string>("Login");
+    const [selectionStudy, setSelectionStudy] = useState<StudyInterface>(showFirstPossibleStudy(ExcutionFaseEnum.SELECTION));
+    const [extractionStudy, setExtractionStudy] = useState<StudyInterface>(showFirstPossibleStudy(ExcutionFaseEnum.EXTRACTION));
 
     return (
-        <AppContext.Provider value={{ button, setButton, item, setItem, renderForm, SetRenderForm }}>
+        <AppContext.Provider value={{ button, setButton, item, setItem, renderForm, setRenderForm, selectionStudy, setSelectionStudy, extractionStudy, setExtractionStudy}}>
             {children}
         </AppContext.Provider>
     );
