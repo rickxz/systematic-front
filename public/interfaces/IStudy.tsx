5f5ab70cf7b4f3b0f32cb8806534750ff712dbf1
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-02 19:33:22 -0300

    fix: Change type of FormAnswers and robAnswers to unknown

--- public/interfaces/IStudy.tsx
+++ public/interfaces/IStudy.tsx
@@ -1,29 +1,27 @@
-import {StudyType} from "./../enums/studyType";
-import {CriteriaType} from "./../enums/CriteriaType";
-import {ReadingPriority} from "./../enums/readingPriority";
-import {SelectionStatus} from "./../enums/selectionStatus";
-import {ExtractionStatus} from "./../enums/extractionStatus";
-
-
+import { StudyType } from "../enums/studyType";
+import { CriteriaType } from "../enums/criteriaType";
+import { ReadingPriority } from "../enums/readingPriority";
+import { SelectionStatus } from "../enums/selectionStatus";
+import { ExtractionStatus } from "../enums/extractionStatus";
 
 export interface StudyInterface {
-    studyId: number;
-    systematicStudyId: number;
-    studyType: StudyType;
-    title: string;
-    year: number;
-    authors: string;
-    venue: string;
-    abstract: string;
-    doi?: string | null;
-    keywords: string[];
-    searchSources: string[];
-    references: string[];
-    criteria: CriteriaType;
-    formAnswers: any[]; // Defina o tipo correto para a resposta do formulário
-    robAnswers: any[]; // Defina o tipo correto para a resposta do ROB
-    comments: string;
-    readingPriority: ReadingPriority;
-    selectionStatus: SelectionStatus;
-    extractionStatus: ExtractionStatus;
+  studyId: number;
+  systematicStudyId: number;
+  studyType: StudyType;
+  title: string;
+  year: number;
+  authors: string;
+  venue: string;
+  abstract: string;
+  doi?: string | null;
+  keywords: string[];
+  searchSources: string[];
+  references: string[];
+  criteria: CriteriaType;
+  formAnswers: unknown[]; // Defina o tipo correto para a resposta do formulário
+  robAnswers: unknown[]; // Defina o tipo correto para a resposta do ROB
+  comments: string;
+  readingPriority: ReadingPriority;
+  selectionStatus: SelectionStatus;
+  extractionStatus: ExtractionStatus;
 }
