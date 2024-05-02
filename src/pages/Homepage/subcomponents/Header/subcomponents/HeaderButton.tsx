f32100c90c84ff9a40c27b959c9b411f5eedce61
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-02 20:09:57 -0300

    fix: correct behavior of sign up an log in button

--- src/pages/Homepage/subcomponents/Header/subcomponents/HeaderButton.tsx
+++ src/pages/Homepage/subcomponents/Header/subcomponents/HeaderButton.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@chakra-ui/react";
-import { Link /*useLocation*/ } from "react-router-dom";
-//import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useContext } from "react";
 import { buttonTheme } from "./styles/ButtonTheme";
-//import AppContext from "../../../../../components/Context/AppContext";
+import AppContext from "../../../../../components/Context/AppContext";
 
 interface IHeaderButton {
   text: string;
@@ -10,18 +10,25 @@ interface IHeaderButton {
   type: string;
 }
 
-export default function HeaderButton({ text, path /*type*/ }: IHeaderButton) {
-  //const location = useLocation();
-  //const context = useContext(AppContext);
-  // const isActive = context?.activeButton === type && location.pathname === "/landing";
+export default function HeaderButton({ text, path, type }: IHeaderButton) {
+  const location = useLocation();
+  const context = useContext(AppContext);
+  const isActive = context?.activeButton === type && location.pathname === "/landing";
+
+  function handleClick() {
+    if (context) {
+      context.setRenderForm(type);
+      context.setActiveButton(type);
+    }
+  }
 
   return (
     <Link to={path}>
       <Button
-        /*color={isActive ? "black" : "white"}*/
-        /*bgColor={/*isActive ? "white" : "rgba(0,0,0,0)"}*/
+        color={isActive ? "black" : "white"}
+        bgColor={isActive ? "white" : "rgba(0,0,0,0)"}
         sx={buttonTheme}
-        onClick={() => {}}
+        onClick={handleClick}
       >
         {text}
       </Button>
