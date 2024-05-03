b941d83a5b35144dfa0c8be661fd606737fa8a3f
Eduardo Derisso <dudu.derisso@gmail.com>
2024-05-03 09:52:25 -0300

    refactor: during the last refactore, the name of the file was changed, so the git understound that a file was deleted

--- public/interfaces/IHeadersTitle.tsx
+++
@@ -1,8 +0,0 @@
-export interface HeadersTitleInterface {
-    title: string;
-    authors: string;
-    year: string;
-    selectionStatus: string;
-    extractionStatus: string;
-    readingPriority: string
-};
