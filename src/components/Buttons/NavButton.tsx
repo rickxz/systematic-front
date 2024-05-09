a7c54cafa7a34b23f294cf4140e46b3217313b3e
gabie762 <gabihenriques821@gmail.com>
2024-05-09 08:30:09 -0300

    changed navbutton border radius

--- src/components/Buttons/NavButton.tsx
+++ src/components/Buttons/NavButton.tsx
@@ -13,26 +13,32 @@ const variants = {
     color: "#E5EBEF",
     hoverBgColor: "#C9D9E5",
     hoverColor: "#526D82",
+    borderRadius: "3px",
   },
   dark: {
     bgColor: "#FDF0D5",
     color: "#301E1A",
     hoverBgColor: "#301E1A",
     hoverColor: "#FDF0D5",
+    borderRadius: "3px",
   },
 }
 
 export default function NavButton({ text, path, variant = "default", ...buttonProps }: iButtonProps) {
-  const { bgColor, color, hoverBgColor, hoverColor } = variants[variant];
+  const { bgColor, color, hoverBgColor, hoverColor, borderRadius } = variants[variant];
   return (
     <Link to={path}>
 
-      <Button bgColor={bgColor} color={color}
-          _hover={{ 
-            bgColor: hoverBgColor,
-            color: hoverColor,
-           }}
-           {...buttonProps}>{text}
+      <Button 
+        bgColor={bgColor} 
+        color={color}
+        borderRadius={borderRadius}
+        _hover={{ 
+          bgColor: hoverBgColor,
+          color: hoverColor,
+        }}
+        {...buttonProps}>
+          {text}
       </Button>
            
     </Link>
