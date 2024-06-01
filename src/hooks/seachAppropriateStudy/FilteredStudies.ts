f98ab457cb586cd9c71a8ca8d455dd7c203ef9ec
Eduardo Derisso <dudu.derisso@gmail.com>
2024-06-01 01:19:58 -0300

    feat: corrigindo tipagem

--- src/hooks/seachAppropriateStudy/FilteredStudies.ts
+++ src/hooks/seachAppropriateStudy/FilteredStudies.ts
@@ -1,20 +1,23 @@
 import { ExtractionStatus } from "../../../public/enums/extractionStatus";
 import { SelectionStatus } from "../../../public/enums/selectionStatus";
 import { StudyInterface } from "../../../public/interfaces/IStudy";
+import { KeywordInterface } from "../../../public/interfaces/KeywordInterface";
 import useFetchStudyData from "./useFetchStudyData";
 import {ExcutionFaseEnum} from "../../../public/enums/ExcutionFaseEnum";
 
 
-export default function FilteredStudies (type: ExcutionFaseEnum): StudyInterface[] {
+export default function FilteredStudies (type: ExcutionFaseEnum): (StudyInterface | KeywordInterface)[] | undefined {
     
     const studies = useFetchStudyData("../../../public/data/NewStudyData.json");
-    var filterStudies: StudyInterface[] = [];
+    let filterStudies: StudyInterface[] = [];
+    console.log(filterStudies)
 
-    if (type == ExcutionFaseEnum.SELECTION) 
-        filterStudies = studies.filter( study => study.selectionStatus === SelectionStatus.INCLUDED);
-    else 
-        filterStudies = studies.filter( study => 
-    study.selectionStatus === SelectionStatus.INCLUDED && study.extractionStatus === ExtractionStatus.INCLUDED);
-
-    return filterStudies;
+    if(studies){
+        if (type == ExcutionFaseEnum.SELECTION) 
+            filterStudies = (studies as StudyInterface[]).filter( study => study.selectionStatus === SelectionStatus.INCLUDED);
+        else 
+            filterStudies = (studies as StudyInterface[]).filter( study => 
+        study.selectionStatus === SelectionStatus.INCLUDED && study.extractionStatus === ExtractionStatus.INCLUDED);
+    }
+    return (filterStudies as StudyInterface[]);
 }
