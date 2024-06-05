c0f1f5227a02ad6ec7446a9ed15b9dac3ef08c44
Eduardo Derisso <dudu.derisso@gmail.com>
2024-06-05 12:15:46 -0300

    fixing[StudySelectionArea]: changing way to render instruction

--- src/pages/Execution/SubPages/Selection/subcomponents/StudySelectionArea.tsx
+++ src/pages/Execution/SubPages/Selection/subcomponents/StudySelectionArea.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import ButtonsForSelection from "./ButtonsForSelection";
 import StudyDataFiel from "../../../../../components/Modals/StudyModal/StudyData";
 import { StudyInterface } from "../../../../../../public/interfaces/IStudy";
@@ -7,13 +7,20 @@ import AppContext from "../../../../../components/Context/AppContext";
 
 export default function StudySelectionArea() {
   const context = useContext(AppContext);
+  const studyData = context?.selectionStudy;
 
+  if (!studyData) return (
+    <Flex mt="10" direction="column" bg="gray.600" w="auto" p="5" alignItems="center">
+      <Text color="white">Clink on a study on the table</Text>
+    </Flex>
+  );
+  
   return (
     <>
       <Flex mt="10" direction="column" bg="gray.600" w="auto" p="5" alignItems={"center"}>
         <ButtonsForSelection />
         <Box w={"100%"} bg="gray.200">
-          <StudyDataFiel studyData={(context?.selectionStudy as StudyInterface)} type="Selection" />
+          <StudyDataFiel studyData={(studyData as StudyInterface)} type="Selection" />
         </Box>
       </Flex>
     </>
