0346f1e91fb600161fba86756b47c1f963443ced
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-07-04 10:50:43 -0300

    feat: added error threatment on useRefreshToken hook

--- src/hooks/validation/useRefreshToken.tsx
+++ src/hooks/validation/useRefreshToken.tsx
@@ -4,18 +4,28 @@ import { useEffect, useState } from "react";
 
 const useRefreshToken = () => {
     const [accessToken, setAccessToken] = useState('');
+    const [refreshed, setRefreshed] = useState(false);
 
     const url = ("http://localhost:8080/");
     const refreshToken = localStorage.getItem('refreshToken');
 
     useEffect(() => {
         async function fetchData(){
-            let response = await axios.post(`${url}api/v1/auth/refresh`, {"refreshToken": refreshToken});
-            setAccessToken(response.data.accessToken);
+            try{
+                let response = await axios.post(`${url}api/v1/auth/refresh`, {"refreshToken": refreshToken});
+                setAccessToken(response.data.accessToken);
+                setRefreshed(true);
+            } catch(err){
+                console.error(err);
+                return err;
+            }
         }
         fetchData();
     }, [])
-    return accessToken;
+    
+    if(refreshed){
+        return accessToken;
+    }
 }
 
 export default useRefreshToken;
