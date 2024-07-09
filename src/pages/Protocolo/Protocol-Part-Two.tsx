98700df7060e3b44f7fce7f18a0435830157efd9
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-07-09 16:14:37 -0300

    feat: handling textTables data

--- src/pages/Protocolo/Protocol-Part-Two.tsx
+++ src/pages/Protocolo/Protocol-Part-Two.tsx
@@ -6,8 +6,20 @@ import TextAreaInput from "../../components/Inputs/InputTextArea";
 import AddTextTable from "../../components/AddDataFields/AddTextTable";
 import AddSelectionTable from "../../components/AddDataFields/AddSelectionTable";
 import FlexLayout from "../../components/ui/Flex/Flex";
+import { useState } from "react";
+
 
 export default function ProtocolPartTwo2() {
+  const [keywords, setKeywords] = useState<string[]>([]);
+  const [InclusionCriteria, setInclusionCriteria] = useState<string[]>([]);
+  const [exclusionCriteria, setExclusionCriteria] = useState<string[]>([]);
+  const [language, setLanguage] = useState<string[]>([])
+
+  async function handleData(){
+    console.log(keywords);
+    console.log(InclusionCriteria);
+    console.log(exclusionCriteria);
+  }
 
   return (
     <FlexLayout defaultOpen={0} navigationType="Accordion">
@@ -20,7 +32,7 @@ export default function ProtocolPartTwo2() {
 
         <FormControl sx={conteiner}>
           <FormControl sx={flex}>
-            <AddTextTable text="Keywords" placeholder="Enter keywords" />
+            <AddTextTable text="Keywords" placeholder="Enter keywords" onUpdate={setKeywords}/>
           </FormControl>
 
           <AddSelectionTable
@@ -29,8 +41,8 @@ export default function ProtocolPartTwo2() {
             typeField="select"
           />
 
-          <AddTextTable text="Inclusion Criteria:" placeholder="Enter the criteria" />
-          <AddTextTable text="Exclusion Criteria:" placeholder="Enter the criteria" />
+          <AddTextTable text="Inclusion Criteria:" placeholder="Enter the criteria" onUpdate={setInclusionCriteria}/>
+          <AddTextTable text="Exclusion Criteria:" placeholder="Enter the criteria" onUpdate={setExclusionCriteria}/>
 
           <AddSelectionTable
             options={["", "Google Scholar", "Scopus", "Scielo", "BDTD", "PubMed"]}
@@ -44,7 +56,7 @@ export default function ProtocolPartTwo2() {
         </FormControl>
 
         <Box sx={btnBox}>
-          <NavButton text="Next" path="/newRevision/protocolpartThree" w={"fit-content"} />
+          <NavButton text="Next" event={handleData} path="/newRevision/protocolpartThree" w={"fit-content"} />
         </Box>
 
       </Box>
