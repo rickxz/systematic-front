5195a61878e09305036ed74218b1556d59545045
Eduardo Derisso <dudu.derisso@gmail.com>
2024-07-16 13:47:27 -0300

    feat[goToUnfinishedSystematicReviewPart]: criando sub funcoes para o hook.

--- src/hooks/goToUnfinishedSystematicReviewPart.tsx
+++ src/hooks/goToUnfinishedSystematicReviewPart.tsx
@@ -1,18 +1,53 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useFetchAllStudies from "./fetch/useFetchAllStudies";
+import useFetchProtocol from "./fetch/useFetchProtocol";
 
-export default function goToUnfinishedSystematicReviewPart() {
-    const navigate =  useNavigate();
+export default async function goToUnfinishedSystematicReviewPart(systematicStudyId: string) {
+    const [protocol, setProtocol] = useState(null);
+    const [studies, setStudies] = useState(null);
+    
+    const protocolData = await useFetchProtocol(systematicStudyId);
+    setProtocol(protocolData);
+                
+    const studiesData = await useFetchAllStudies(systematicStudyId);
+    setStudies(studiesData);
 
-    const stillInProtocolOne = protocolPartOneIsFinished();
-    const stillInProtocolTwo = protocolPartTwoIsFinished();
-    const stillInProtocolThree = protocolPartThreeIsFinished();
-    const stillInSelection = SelectionIsFinished();
-    const stillInExtraction = ExtractionIsFinished();
+    console.log(protocol);
+    console.log(studies);
+    
 
-    if (stillInProtocolOne) navigate("/newRevision/protocol");
-    else if (stillInProtocolTwo) navigate("/newRevision/protocolpartTwo");
-    else if (stillInProtocolThree) navigate("/newRevision/protocolpartThree");
-    else if (stillInSelection) navigate("/newRevision/selection");
-    else if (stillInExtraction) navigate("/newRevision/extraction");
-    else navigate("/newRevision/finalization");
+    function protocolPartOneIsFinished(protocol: any): boolean {
+        return false;
+    }
+
+    function protocolPartTwoIsFinished(protocol: any): boolean {
+        return false;
+    }
+
+    function protocolPartThreeIsFinished(protocol: any): boolean {
+        return false;
+    }
+
+    function SelectionIsFinished(studies: any): boolean {
+        return false;
+    }
+
+    function ExtractionIsFinished(studies: any): boolean {
+        return false;
+    }
+
+
+    const stillInProtocolOne = protocolPartOneIsFinished(protocol);
+    const stillInProtocolTwo = protocolPartTwoIsFinished(protocol);
+    const stillInProtocolThree = protocolPartThreeIsFinished(protocol);
+    const stillInSelection = SelectionIsFinished(studies);
+    const stillInExtraction = ExtractionIsFinished(studies);
+
+    if (stillInProtocolOne) console.log("/newRevision/protocol");
+    else if (stillInProtocolTwo) console.log("/newRevision/protocolpartTwo");
+    else if (stillInProtocolThree) console.log("/newRevision/protocolpartThree");
+    else if (stillInSelection) console.log("/newRevision/selection");
+    else if (stillInExtraction) console.log("/newRevision/extraction");
+    else console.log("/newRevision/finalization");
 }
