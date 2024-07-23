03c535ce8c89bdfcb56613a162b88188e5a6536b
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-07-23 17:31:32 -0300

    fix: remove unuseless logs of createProtocolPartThree

--- src/hooks/revisions/useCreateProtocolThree.tsx
+++ src/hooks/revisions/useCreateProtocolThree.tsx
@@ -4,17 +4,14 @@ const useCreateProtocolTwo = async (analysis: string, id: string) => {
 
     const data = {
         analysisAndSynthesisProcess: analysis
-    }
+    };
 
     console.log(data);
 
-    const url = `http://localhost:8080/systematic-study/${id}/protocol`
+    const url = `http://localhost:8080/systematic-study/${id}/protocol`;
 
     try{
-        let response = await axios.put(url, data, {withCredentials: true});
-        console.log(response);
-        let response2 = await axios.get(url, {withCredentials: true});
-        console.log(response2);
+        await axios.put(url, data, {withCredentials: true});
     }
     catch(err){
         console.log(err);
