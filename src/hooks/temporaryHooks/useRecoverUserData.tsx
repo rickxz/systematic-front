0c6c70fe2a1e337ca43712b5ac7dc8654bb827e6
Eduardo Derisso <dudu.derisso@gmail.com>
2024-07-25 20:13:51 -0300

    fixing[useRecoverUserData]: rewriting hook to return directilly the username and not a function

--- src/hooks/temporaryHooks/useRecoverUserData.tsx
+++ src/hooks/temporaryHooks/useRecoverUserData.tsx
@@ -1,10 +1,5 @@
 export default function useRecoverUserData() {
-
-    function recoverUserData() {
-        const userData = localStorage.getItem("username");
-        console.log(userData);
-        return userData;
-    }
-
-    return recoverUserData;
+    const userData = localStorage.getItem("username");
+    console.log(userData);
+    return userData;
 }
