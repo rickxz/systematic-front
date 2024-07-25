6ea19a6cf39ed87cd3dd8501af96c9acdcffffae
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-07-25 13:38:09 -0300

    feat: cardInfo adjusted to receive the reviewId

--- src/pages/UserArea/subcomponents/CardInfos.tsx
+++ src/pages/UserArea/subcomponents/CardInfos.tsx
@@ -1,15 +1,18 @@
 import { Box, Text } from "@chakra-ui/react";
+import { Link as ChakraLink, LinkProps } from '@chakra-ui/react'
+import { Link as ReactRouterLink } from 'react-router-dom'
 import { conteiner, text } from "../styles/CardInfosStyle";
 
 interface ICardInfosProps {
   title: string;
   reviewers: string[];
+  reviewId: string;
 }
 
-export default function CardInfos({ title, reviewers }: ICardInfosProps) {
+export default function CardInfos({ title, reviewers, reviewId }: ICardInfosProps) {
   return (
     <Box sx={conteiner}>
-      <Text sx={text}>{title}</Text>
+      <ChakraLink as={ReactRouterLink} to={`/protocol/${reviewId}`} sx={text}>{title}</ChakraLink>
       <Text>Reviewers: {reviewers} </Text>
     </Box>
   );
