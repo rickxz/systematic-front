f249da3086498aa9e90197715d9a0d7229b6e27c
Eduardo Derisso <dudu.derisso@gmail.com>
2024-07-25 23:10:15 -0300

    refactore[useRecoverUserData]: removing unecessary imports

--- src/hooks/temporaryHooks/useRecoverUserData.tsx
+++ src/hooks/temporaryHooks/useRecoverUserData.tsx
@@ -2,7 +2,6 @@ import { useVerifyIfLoggedIn } from "./useVerifyIfLoggedIn";
 
 export default function useRecoverUserData() {
     const userData = localStorage.getItem("username");
-    console.log(userData);
     const isLoggednIn =  useVerifyIfLoggedIn();
 
     if (isLoggednIn) return userData;
