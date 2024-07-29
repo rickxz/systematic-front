746ed9351d1f40ca20ce5d4d856cd4bb56741d80
Eduardo Derisso <dudu.derisso@gmail.com>
2024-07-29 14:29:58 -0300

    feat[RevisionCard: add redirection to certains pages based on witch part the researcher stoped his or her work

--- src/pages/UserArea/subcomponents/RevisionCard.tsx
+++ src/pages/UserArea/subcomponents/RevisionCard.tsx
@@ -20,13 +20,52 @@ export default function RevisionCard({ revisionId, id, title, reviewers, status,
   async function redirectToReview(){
     const url = `http://localhost:8080/systematic-study/${revisionId}/protocol`;
     let response = await axios.get(url, {withCredentials: true})
-    console.log(response);
-    /*
+    //primeira página
+    console.log(response.data.content.id);
+    console.log(response.data.content.systematicStudy);
+
+    console.log(response.data.content.goal);
+    console.log(response.data.content.justification);
+
+    console.log(response.data.content.studiesLanguages);
+    console.log(response.data.content.eligibilityCriteria);
+    console.log(response.data.content.informationSources);
+    console.log(response.data.content.keywords);
+    console.log(response.data.content.sourcesSelectionCriteria);
+    console.log(response.data.content.searchMethod);
+    console.log(response.data.content.extractionQuestions);
+    console.log(response.data.content.picoc);
+    console.log(response.data.content.robQuestions);
+    console.log(response.data.content.selectionProcess);
+    console.log(response.data.content.studyTypeDefinition);
+    console.log(response.data.content.dataCollectionProcess);
+
+    console.log(response.data.content.researchQuestions);
+    console.log(response.data.content.analysisAndSynthesisProcess);
+
+    console.log(response.data.content.searchString);
+
+    
     if(response.data.content.goal == null || response.data.content.justification == null){
-      //window.location.href = `http://localhost:5173/#/newRevision/protocol/${revisionId}`;
+      window.location.href = `http://localhost:5173/#/newRevision/protocol/${revisionId}`;
     }
-    else console.log("continuar lógica de continuar protocolo");
-    */
+    else if (response.data.content.studiesLanguages == null ||
+              response.data.content.eligibilityCriteria == null ||
+              response.data.content.informationSources == null ||
+              response.data.content.keywords == null ||
+              response.data.content.sourcesSelectionCriteria == null ||
+              response.data.content.searchMethod == null ||
+              response.data.content.extractionQuestions == null ||
+              response.data.content.picoc == null ||
+              response.data.content.robQuestions == null ||
+              response.data.content.selectionProcess == null ||
+              response.data.content.studyTypeDefinition == null ||
+              response.data.content.dataCollectionProcess == null)
+              
+              window.location.href = ` http://localhost:5173/#/newRevision/protocolpartTwo/${revisionId}`;
+    else if ( response.data.content.researchQuestions == null ||
+              response.data.content.analysisAndSynthesisProcess == null)  
+      window.location.href = ` http://localhost:5173/#/newRevision/selection`;   
   }
   
   return (
