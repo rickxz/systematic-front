9c7fcb22c9b7fb59775751f3f8b7c34058f354a3
Eduardo Derisso <dudu.derisso@gmail.com>
2024-07-30 08:13:10 -0300

    feat[RevisionCard, goToUnfinishedSystematicReviewPart]: desenvolved a especific hook to deal with redirections based in witch part of the work the research stoped

--- src/hooks/goToUnfinishedSystematicReviewPart.tsx
+++ src/hooks/goToUnfinishedSystematicReviewPart.tsx
@@ -1,53 +1,60 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import useFetchAllStudies from "./fetch/useFetchAllStudies";
 import useFetchProtocol from "./fetch/useFetchProtocol";
+import { AxiosResponse } from "axios";
+import {Protocol} from "../../public/interfaces/protocolInterface";
 
-export default async function goToUnfinishedSystematicReviewPart(systematicStudyId: string) {
-    const [protocol, setProtocol] = useState(null);
-    const [studies, setStudies] = useState(null);
+function isProtocolPartOneFinished(response:  Protocol) {
+    console.log(response);
+    return response.goal == null || response.justification == null
+}
     
-    const protocolData = await useFetchProtocol(systematicStudyId);
-    setProtocol(protocolData);
+function isProtocolPartTwoFinished(response:  Protocol) {
+    return response.studiesLanguages == null ||
+        response.eligibilityCriteria == null ||
+        response.informationSources == null ||
+        response.keywords == null ||
+        response.sourcesSelectionCriteria == null ||
+        response.searchMethod == null ||
+        response.extractionQuestions == null ||
+        response.picoc == null ||
+        response.robQuestions == null ||
+        response.selectionProcess == null ||
+        response.studyTypeDefinition == null ||
+        response.dataCollectionProcess == null
+    }
+
+function isProtocolPartThreeFinished(response:  Protocol) {
+    return response.researchQuestions == null ||
+           response.analysisAndSynthesisProcess == null
+    }
+
+function isSelectionProcessFinished(response:  AxiosResponse<any, any>) { return true; }
+
+function isExtractionProcessFinished(response:  AxiosResponse<any, any>) { return true; }
+
+
+
+
+
+export default async function goToUnfinishedSystematicReviewPart(revisionId: string) {
+    const protocolData = await useFetchProtocol(revisionId);
+    const studiesData = await useFetchAllStudies(revisionId);
+
+    
+    if(isProtocolPartOneFinished(protocolData)) {
+        window.location.href = `http://localhost:5173/#/newRevision/protocol/${revisionId}`;
+      }
+      else if (isProtocolPartTwoFinished(protocolData)) {
+                  window.location.href = ` http://localhost:5173/#/newRevision/protocolpartTwo/${revisionId}`;
+                }
                 
-    const studiesData = await useFetchAllStudies(systematicStudyId);
-    setStudies(studiesData);
-
-    console.log(protocol);
-    console.log(studies);
-    
-
-    function protocolPartOneIsFinished(protocol: any): boolean {
-        return false;
-    }
-
-    function protocolPartTwoIsFinished(protocol: any): boolean {
-        return false;
-    }
-
-    function protocolPartThreeIsFinished(protocol: any): boolean {
-        return false;
-    }
-
-    function SelectionIsFinished(studies: any): boolean {
-        return false;
-    }
-
-    function ExtractionIsFinished(studies: any): boolean {
-        return false;
-    }
-
-
-    const stillInProtocolOne = protocolPartOneIsFinished(protocol);
-    const stillInProtocolTwo = protocolPartTwoIsFinished(protocol);
-    const stillInProtocolThree = protocolPartThreeIsFinished(protocol);
-    const stillInSelection = SelectionIsFinished(studies);
-    const stillInExtraction = ExtractionIsFinished(studies);
-
-    if (stillInProtocolOne) console.log("/newRevision/protocol");
-    else if (stillInProtocolTwo) console.log("/newRevision/protocolpartTwo");
-    else if (stillInProtocolThree) console.log("/newRevision/protocolpartThree");
-    else if (stillInSelection) console.log("/newRevision/selection");
-    else if (stillInExtraction) console.log("/newRevision/extraction");
-    else console.log("/newRevision/finalization");
+      else if (isProtocolPartThreeFinished(protocolData)) {
+        window.location.href = ` http://localhost:5173/#/newRevision/protocolpartThree/${revisionId}`;
+      }
+        
+      else if(isSelectionProcessFinished(studiesData)) {
+        window.location.href = ` http://localhost:5173/#/newRevision/selection`;
+      }
+      else if (isExtractionProcessFinished(studiesData)) window.location.href = ` http://localhost:5173/#/newRevision/extraction`;
+      else window.location.href = ` http://localhost:5173/#/newRevision/finalization`;
 }
--- src/pages/UserArea/subcomponents/RevisionCard.tsx
+++ src/pages/UserArea/subcomponents/RevisionCard.tsx
@@ -5,6 +5,7 @@ import { Box, Card } from "@chakra-ui/react";
 import EnterRevisionButton from "./EnterRevisionButton";
 import { CardInfosConteiner, Cardstyles } from "../styles/revisionCardstyles";
 import axios from "../../../interceptor/interceptor";
+import goToUnfinishedSystematicReviewPart from "../../../hooks/goToUnfinishedSystematicReviewPart";
 
 interface iRevisionCardProps {
   revisionId: string,
@@ -18,73 +19,7 @@ interface iRevisionCardProps {
 
 export default function RevisionCard({ revisionId, id, title, reviewers, status, creation, /* isEdited */}: iRevisionCardProps) {
   async function redirectToReview(){
-    const url = `http://localhost:8080/systematic-study/${revisionId}/protocol`;
-    let response = await axios.get(url, {withCredentials: true})
-    /*
-    console.log(response.data.content.id);
-    console.log(response.data.content.systematicStudy);
-
-    console.log(response.data.content.goal);
-    console.log(response.data.content.justification);
-
-    console.log(response.data.content.studiesLanguages);
-    console.log(response.data.content.eligibilityCriteria);
-    console.log(response.data.content.informationSources);
-    console.log(response.data.content.keywords);
-    console.log(response.data.content.sourcesSelectionCriteria);
-    console.log(response.data.content.searchMethod);
-    console.log(response.data.content.extractionQuestions);
-    console.log(response.data.content.picoc);
-    console.log(response.data.content.robQuestions);
-    console.log(response.data.content.selectionProcess);
-    console.log(response.data.content.studyTypeDefinition);
-    console.log(response.data.content.dataCollectionProcess);
-
-    console.log(response.data.content.researchQuestions);
-    console.log(response.data.content.analysisAndSynthesisProcess);
-
-    console.log(response.data.content.searchString);
-    */
-
-    function isProtocolPartOneFinished() {return response.data.content.goal == null || response.data.content.justification == null}
-    
-    function isProtocolPartTwoFinished() {return response.data.content.studiesLanguages == null ||
-      response.data.content.eligibilityCriteria == null ||
-      response.data.content.informationSources == null ||
-      response.data.content.keywords == null ||
-      response.data.content.sourcesSelectionCriteria == null ||
-      response.data.content.searchMethod == null ||
-      response.data.content.extractionQuestions == null ||
-      response.data.content.picoc == null ||
-      response.data.content.robQuestions == null ||
-      response.data.content.selectionProcess == null ||
-      response.data.content.studyTypeDefinition == null ||
-      response.data.content.dataCollectionProcess == null}
-
-      function isProtocolPartThreeFinished() {return response.data.content.researchQuestions == null ||
-        response.data.content.analysisAndSynthesisProcess == null}
-
-      function isSelectionProcessFinished() { return true; }
-
-      function isExtractionProcessFinished() { return true; }
-
-    
-    if(isProtocolPartOneFinished()) {
-      window.location.href = `http://localhost:5173/#/newRevision/protocol/${revisionId}`;
-    }
-    else if (isProtocolPartTwoFinished()) {
-                window.location.href = ` http://localhost:5173/#/newRevision/protocolpartTwo/${revisionId}`;
-              }
-              
-    else if (isProtocolPartThreeFinished()) {
-      window.location.href = ` http://localhost:5173/#/newRevision/protocolpartThree/${revisionId}`;
-    }
-      
-    else if(isSelectionProcessFinished()) {
-      window.location.href = ` http://localhost:5173/#/newRevision/selection`;
-    }
-    else if (isExtractionProcessFinished()) window.location.href = ` http://localhost:5173/#/newRevision/extraction`;
-    else window.location.href = ` http://localhost:5173/#/newRevision/finalization`;
+    goToUnfinishedSystematicReviewPart(revisionId);
   }
   
   return (
