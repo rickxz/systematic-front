442e2e85e93b2f6473003edc90e50fd149379d38
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-08-02 15:54:02 -0300

    feat: pickList modal added

---
+++ src/components/Modals/Data Extraciton Field Creation/PickListModal.tsx
@@ -0,0 +1,32 @@
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react"
+import { useDisclosure } from "@chakra-ui/react"
+import { useEffect } from "react";
+import AddTextTable from "../../AddDataFields/AddTextTable";
+
+interface Props{
+    id: string;
+    url: string;
+}
+
+function PickListModal({id, url}: Props) {
+    const { isOpen, onClose, onOpen } = useDisclosure();
+    
+    useEffect(() => {
+        onOpen();
+    }, [])
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Insert the options</ModalHeader>
+                <ModalBody>
+                    <AddTextTable text="Options" placeholder="Options here" id={id} url={url}/>
+                </ModalBody>
+                <ModalCloseButton />
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default PickListModal
