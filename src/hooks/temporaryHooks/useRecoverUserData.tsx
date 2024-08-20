f5c7dfe4e5695dea96b944f8247e46e534632ae9
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-20 11:51:04 -0300

    fixing[useRecoverUserData]: correcting problens on checking if user still logged in

--- src/hooks/temporaryHooks/useRecoverUserData.tsx
+++ src/hooks/temporaryHooks/useRecoverUserData.tsx
@@ -3,9 +3,18 @@ import { useVerifyIfLoggedIn } from "./useVerifyIfLoggedIn";
 export default function useRecoverUserData() {
     const userData = localStorage.getItem("username");
     const verifyIfLoggedInResponse =  useVerifyIfLoggedIn();
+    let isLoggedIn = verifyIfLoggedInResponse.isLoggedIn;
+    let isChecking = verifyIfLoggedInResponse.isChecking;
     
-    if (verifyIfLoggedInResponse.isLoggedIn) return userData;
-
-    localStorage.clear();
-    return null;
+    if (isChecking) {
+        console.log("waiting to checking finish")
+        return;
+    }
+    else if (!isLoggedIn) {
+        console.log("user not logged in")
+        localStorage.clear();
+        return null
+    }
+    console.log("user logged in")
+    return userData;
 }
