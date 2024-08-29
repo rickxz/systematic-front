791565ee430764c365a4f61f64d4a1ea91ece3c8
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-08-29 07:28:00 -0300

    fix: removed the interrogation point of the url

--- src/hooks/validation/useHandleRegister.tsx
+++ src/hooks/validation/useHandleRegister.tsx
@@ -109,7 +109,6 @@ const useHandleRegister = (closeModal: () => void) => {
                     });
 
                     closeModal();
-                    setTimeout(() => (e.target as HTMLFormElement).submit(), 0);
                 }
             } catch (err: any) {
                 console.error(err);
