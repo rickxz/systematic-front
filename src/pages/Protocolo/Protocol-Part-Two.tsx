98aed47f3ed9a90873ca6da7e4de335d82dacd40
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-08-31 12:58:45 -0300

    feat: all the logic of Protoco-part-two.tsx removed, and added spaces between components

--- src/pages/Protocolo/Protocol-Part-Two.tsx
+++ src/pages/Protocolo/Protocol-Part-Two.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import axios from '../../interceptor/interceptor';
 import Header from "../../components/ui/Header/Header";
 import NavButton from "../../components/Buttons/NavButton";
 import { Progress, FormControl, Box, Checkbox, Flex } from "@chakra-ui/react";
@@ -8,148 +6,65 @@ import TextAreaInput from "../../components/Inputs/InputTextArea";
 import AddTextTable from "../../components/AddDataFields/AddTextTable";
 import AddSelectionTable from "../../components/AddDataFields/AddSelectionTable";
 import FlexLayout from "../../components/ui/Flex/Flex";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import useCreateProtocolTwo from "../../hooks/reviews/useCreateProtocolTwo";
-import { useNavigate } from "react-router-dom";
 
 export default function ProtocolPartTwo2() {
-  const [researchStrategy, setResearchStrategy] = useState<string>('');
-  const [selectProcess, setSelectProcess] = useState<string>('');
-  const [sourcesSelectionCriteria, setSourcesSelectionCriteria] = useState<string>('');
-  const [studyTypeDefinition, setStudyTypeDefinition] = useState<string>('');
-  const [searchString, setSearchString] = useState<string>('');
-  const [showMoreQuestionComponents, setShowMoreQuestioComponents] = useState<boolean>(false);
-  const [dataCollectionProcess, setDataCollectionProcess] = useState<string>("");
-  const [researchQuestions, setResearchQuestions] = useState<string>("");
-  const { id = '' } = useParams();
-
-  const navigate = useNavigate();
-  const url = `http://localhost:8080/systematic-study/${id}/protocol`;
-
-  useEffect(() => {
-    async function fetchValues(){
-      const accessToken = localStorage.getItem('accessToken');
-      let options = {
-        headers: { Authorization: `Bearer ${accessToken}` }
-      }
-
-      const url = `http://localhost:8080/systematic-study/${id}/protocol`;
-      let response = await axios.get(url, options);
-
-      setSelectProcess(response.data.content.selectionProcess);
-      setResearchStrategy(response.data.content.searchMethod);
-      setSourcesSelectionCriteria(response.data.content.sourcesSelectionCriteria);
-      setStudyTypeDefinition(response.data.content.studyTypeDefinition);
-      setSearchString(response.data.content.searchString);
-      setDataCollectionProcess(response.data.content.dataCollectionProcess);
-      setResearchQuestions(response.data.content.researchQuestions);
-      
-    }
-
-    fetchValues();
-  }, [])
-
-  async function handleData(){
-    await  useCreateProtocolTwo(researchStrategy, selectProcess, sourcesSelectionCriteria, studyTypeDefinition, searchString, dataCollectionProcess, id);
-    navigate(`/newRevision/protocolpartThree/${id}`);
-  }
-
-  async function handleDataReturn(){
-    await  useCreateProtocolTwo(researchStrategy, selectProcess, sourcesSelectionCriteria, studyTypeDefinition, searchString, dataCollectionProcess, id);
-    navigate(`/newRevision/protocol/${id}`);
-  }
-
-  function handleResearchStrategy(e: React.ChangeEvent<HTMLTextAreaElement>){
-    setResearchStrategy(e.target.value);
-  }
-
-  function handleSelectProcess(e: React.ChangeEvent<HTMLTextAreaElement>){
-    setSelectProcess(e.target.value);
-  }
-
-  function handleSourcesSelectionCriteria(e: React.ChangeEvent<HTMLTextAreaElement>){
-    setSourcesSelectionCriteria(e.target.value);
-  }
-
-  function handleStudyTypeDefinition(e: React.ChangeEvent<HTMLTextAreaElement>){
-    setStudyTypeDefinition(e.target.value);
-  }
-
-  function handleSearchString(e: React.ChangeEvent<HTMLTextAreaElement>){
-    setSearchString(e.target.value);
-  }
-
-  function handleDataCollectionProcess(e: React.ChangeEvent<HTMLTextAreaElement>){
-    setDataCollectionProcess(e.target.value);
-  }
-
-
-  function handleMoreResearchQuestionArea() {
-    if (!showMoreQuestionComponents)
-      setShowMoreQuestioComponents(true);
-    else 
-      setShowMoreQuestioComponents(false);
-  }
 
   return (
     <FlexLayout defaultOpen={0} navigationType="Accordion">
       <Box w={"100%"} display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
         <Header text="Protocol" />
         <Progress value={66} w={"100%"} color={"black"} />
+
         <FormControl sx={conteiner}>
           
-          <TextAreaInput value={searchString} onChange={handleSearchString}  label="Search String" placeholder="Enter the search string" />
-          <TextAreaInput value={studyTypeDefinition} onChange={handleStudyTypeDefinition}  label="Study Type Definition" placeholder="Enter the study type definition" />
-          <TextAreaInput value={dataCollectionProcess} onChange={handleDataCollectionProcess}  label="Data Colletion Process" placeholder="Enter the data colletion process" />
+          <TextAreaInput value={''} onChange={(e) => {e}}  label="Search String" placeholder="Enter the search string" />
+          
+          <TextAreaInput value={''} onChange={(e) => {e}}  label="Study Type Definition" placeholder="Enter the study type definition" />
+          
+          <TextAreaInput value={''} onChange={(e) => {e}}  label="Data Colletion Process" placeholder="Enter the data colletion process" />
 
+          <Flex>
+            <AddTextTable url={''} id={''} text="Research Questions" placeholder="Enter the other Research Questions"/>
+          </Flex>
 
-          { researchQuestions.length != 0 ?
-             <Flex>
-              <AddTextTable url={url} id={id} text="Research Questions" placeholder="Enter the other Research Questions"/>
-            </Flex>
-          :
-            <>
-              <Checkbox  onChange={handleMoreResearchQuestionArea}>Add more reasearch questions</Checkbox>
-              {showMoreQuestionComponents && (
-                  <Flex>
-                    <AddTextTable url={url} id={id} text="Research Questions" placeholder="Enter the other Research Questions"/>
-                  </Flex>
-              )}
-            </>
-          }
           <FormControl sx={flex}>
-            <AddTextTable url={url} id={id} text="Keywords" placeholder="Enter the keywords related to your review"/>
+            <AddTextTable url={''} id={''} text="Keywords" placeholder="Enter the keywords related to your review"/>
           </FormControl>
+
           <AddSelectionTable
             label="Languages"
             type="studiesLanguages"
-            url={url}
+            url={''}
             options={["ENGLISH", "PORTUGUESE", "FRENCH", "SPANISH", "GERMAN"]}
             placeholder={"Select language"}
             typeField="select"
           />
-          <AddTextTable id={id} text="Inclusion criteria" placeholder="Enter the criteria" url={url}/>        
-          <AddTextTable id={id} text="Exclusion criteria" placeholder="Enter the criteria" url={url}/>
+
+          <AddTextTable id={''} text="Inclusion criteria" placeholder="Enter the criteria" url={''}/>        
+         
+          <AddTextTable id={''} text="Exclusion criteria" placeholder="Enter the criteria" url={''}/>
           
-          <TextAreaInput value={sourcesSelectionCriteria} onChange={handleSourcesSelectionCriteria}  label="Sources Selection Criteria" placeholder="Enter the sources selection criteria" />
+          <TextAreaInput value={''} onChange={() => {}}  label="Sources Selection Criteria" placeholder="Enter the sources selection criteria" />
+         
           <AddSelectionTable
             label="Databases and Infortion Source"
             type="databases" 
-            url={url}
+            url={''}
             options={["Google Scholar", "Scopus", "Scielo", "BDTD", "PubMed", "Expert Suggestion",
                "Backward Snowballing", "Forward Snowballing", "Grey Literature Sources"]}
             placeholder={"Select Data Base"}
             typeField="select"
           />
 
-          <TextAreaInput value={researchStrategy} onChange={handleResearchStrategy} label="Research Strategy" placeholder="Enter research strategy" />
-          <TextAreaInput value={selectProcess} onChange={handleSelectProcess} label="Article Selection Process" placeholder="Enter selection process" />
+          <TextAreaInput value={''} onChange={(e) => {e}} label="Research Strategy" placeholder="Enter research strategy" />
+          
+          <TextAreaInput value={''} onChange={(e) => {e}} label="Article Selection Process" placeholder="Enter selection process" />
 
         </FormControl>
+       
         <Box sx={btnBox}>
-          <NavButton event={handleDataReturn} text='Return'/>
-          <NavButton event={handleData} text="Next" />
+          <NavButton event={async () => {}} text='Return'/>
+          <NavButton event={async () => {}} text="Next" />
         </Box>
       </Box>
     </FlexLayout>
