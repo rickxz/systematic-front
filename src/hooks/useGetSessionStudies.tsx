afa939a283d70d00f7bcb8d9a88f9200de53ba34
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-14 11:59:58 -0300

    refactor: Changed the {prop: type} type declaration on useGetSessionStudies by its interface

--- src/hooks/useGetSessionStudies.tsx
+++ src/hooks/useGetSessionStudies.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "../interceptor/interceptor";
+import ArticleInterface from "../../public/interfaces/ArticleInterface";
 
 const useGetSessionStudies = (sessionId:string) => {
     const reviewId = localStorage.getItem('systematicReviewId');
     const token = localStorage.getItem('accessToken');
     const url = `http://localhost:8080/api/v1/systematic-study/${reviewId}/find-by-search-session/${sessionId}`;
-    const [articles, setArticles] = useState<{title: string, authors: string, venue: string} []>([]);
+    const [articles, setArticles] = useState<ArticleInterface[]>([]);
+
     const options = {
         headers: { Authorization: `Bearer ${token}` }
     }
