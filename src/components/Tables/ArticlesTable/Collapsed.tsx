be42ba9b8806322d7e628c1d31e0f6e34309e148
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-14 13:13:36 -0300

    refactor: removed extra () and uselessCode from Collapsed

--- src/components/Tables/ArticlesTable/Collapsed.tsx
+++ src/components/Tables/ArticlesTable/Collapsed.tsx
@@ -8,110 +8,72 @@ interface Props {
 
 export default function Collapsed({articles}: Props) {
   return (
-    (
-        <TableContainer 
-            width={"80%"}
-            mt={5}  
-            borderRadius="15px 15px 0 0" 
-            boxShadow="lg" 
-            bg="#EBF0F3"
+    <TableContainer 
+        width={"80%"}
+        mt={5}  
+        borderRadius="15px 15px 0 0" 
+        boxShadow="lg" 
+        bg="#EBF0F3"
+    >
+        <Table
+            variant="simple"
+            colorScheme="#263C56"
+            size="md"
+            boxShadow="md"
         >
-            <Table
-                variant="simple"
-                colorScheme="#263C56"
-                size="md"
-                boxShadow="md"
-            >
-                <Thead bg="#263C56" borderRadius={10}>
-                    <Tr>
-                        <Th textAlign='center' color="white">ID Paper</Th>
-                        <Th textAlign='center' color="white">Title</Th>
-                        <Th textAlign='center' color="white">Author</Th>
-                        <Th textAlign='center' color="white">Journal</Th>
-                    </Tr>
+            <Thead bg="#263C56" borderRadius={10}>
+                <Tr>
+                    <Th textAlign='center' color="white">ID Paper</Th>
+                    <Th textAlign='center' color="white">Title</Th>
+                    <Th textAlign='center' color="white">Author</Th>
+                    <Th textAlign='center' color="white">Journal</Th>
+                </Tr>
+                
+            </Thead>
+            <Tbody>
+                {articles ? articles.map((e, index) => <Tr
+                    key={index}
+                    _hover={{ bg: "#F5F8F9" }}
+                    transition="background-color 0.3s, box-shadow 0.3s"
+                >
+
+                    <Td sx={tdSX}>{index + 1}</Td>
                     
-                </Thead>
-                <Tbody>
-                    {/*     <Tr
-                            key={index}
-                            _hover={{ bg: "teal.50", boxShadow: "md" }}
-                            transition="background-color 0.3s, box-shadow 0.3s"
+                    <Td sx={tdSX}>
+                        <Tooltip label={e.title} aria-label="Título completo"
+                        hasArrow
+                        placement="right" // Pode ser "top", "bottom", "left", "right"
+                        fontSize="xs" // Tamanho da fonte
+                        p={3} // Padding do tooltip>
                         >
-                            <Td>{article.id}</Td>
-                            <Td>{article.title}</Td>
-                            <Td>{article.author}</Td>
-                            <Td>{article.journal}</Td>
-                        </Tr>
-                    ))} */}
+                            <Text sx={tdSX}>{e.title}</Text>
+                        </Tooltip>
+                    </Td>
 
-                    {articles ? articles.map((e, index) => <Tr
-                        key={index}
-                        _hover={{ bg: "#F5F8F9" }}
-                        transition="background-color 0.3s, box-shadow 0.3s"
-                    >
-
-                        <Td sx={tdSX}>{index + 1}</Td>
-                        
-                        <Td sx={tdSX}>
-                            <Tooltip label={e.title} aria-label="Título completo"
+                    <Td sx={tdSX}>
+                        <Tooltip label={e.authors} aria-label="Título completo"
                             hasArrow
                             placement="right" // Pode ser "top", "bottom", "left", "right"
                             fontSize="xs" // Tamanho da fonte
                             p={3} // Padding do tooltip>
                             >
-                                <Text sx={tdSX}>{e.title}</Text>
-                            </Tooltip>
-                        </Td>
-
-                        <Td sx={tdSX}>
-                            <Tooltip label={e.authors} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={tdSX}>{e.authors}</Text>
-                            </Tooltip>
-                        </Td>
-                        <Td sx={tdSX}>
-                            <Tooltip label={e.venue} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={tdSX}>{e.venue}</Text>
-                            </Tooltip>
-                        </Td>
-                        
-                    </Tr>) : <p>no articles Found</p>}
-                    {/* <Tr
-                        _hover={{ bg: "#F5F8F9" }}
-                        transition="background-color 0.3s, box-shadow 0.3s"
-                    >
-
-                        <Td>2</Td>
-                        <Td>title</Td>
-                        <Td>author</Td>
-                        <Td>Journal</Td>
-                        
-                    </Tr>
-                    <Tr
-                        _hover={{ bg: "#F5F8F9" }}
-                        transition="background-color 0.3s,box-shadow 0.3s"
-                    >
-
-                        <Td>3</Td>
-                        <Td>title</Td>
-                        <Td>author</Td>
-                        <Td>Journal</Td>
-                        
-                    </Tr> */}
-
-
-                </Tbody>
-            </Table>
-        </TableContainer>
-    )
+                                <Text sx={tdSX}>{e.authors}</Text>
+                        </Tooltip>
+                    </Td>
+                    <Td sx={tdSX}>
+                        <Tooltip label={e.venue} aria-label="Título completo"
+                            hasArrow
+                            placement="right" // Pode ser "top", "bottom", "left", "right"
+                            fontSize="xs" // Tamanho da fonte
+                            p={3} // Padding do tooltip>
+                            >
+                                <Text sx={tdSX}>{e.venue}</Text>
+                        </Tooltip>
+                    </Td>
+                    
+                </Tr>) : <p>no articles Found</p>}
+            </Tbody>
+        </Table>
+    </TableContainer>
   )
 }
--- src/components/Tables/ArticlesTable/Expanded.tsx
+++ src/components/Tables/ArticlesTable/Expanded.tsx
@@ -8,125 +8,123 @@ interface Props {
 
 export default function Expanded({articles}: Props) {
   return (
-    (
-        <TableContainer 
-            width={"80%"}
-            mt={5}  
-            borderRadius="15px 15px 0 0" 
-            boxShadow="lg" 
-            bg="#EBF0F3"
+    <TableContainer 
+        width={"80%"}
+        mt={5}  
+        borderRadius="15px 15px 0 0" 
+        boxShadow="lg" 
+        bg="#EBF0F3"
+    >
+        <Table
+            variant="simple"
+            colorScheme="#263C56"
+            size="md"
+            boxShadow="md"
         >
-            <Table
-                variant="simple"
-                colorScheme="#263C56"
-                size="md"
-                boxShadow="md"
-            >
-                <Thead bg="#263C56" borderRadius={10}>
-                    <Tr>
-                        <Th w={'5rem'} textAlign='center' color="white">ID Paper</Th>
-                        <Th w={'5rem'} textAlign='center' color="white">Title</Th>
-                        <Th w={'5rem'} textAlign='center' color="white">Author</Th>
-                        <Th w='5rem' textAlign='center' color="white">Year</Th>
-                        <Th w='5rem' textAlign='center' color="white">Selection</Th>
-                        <Th w='5rem' textAlign='center' color="white">Extraction</Th>
-                        <Th w={'5rem'} textAlign='center'  color="white">Reading priority</Th>
-                        {/* 
-                        <Th color="white">Status/Selection</Th>
-                        <Th color="white">Status/Extraction</Th>
-                        <Th color="white">Reading Priority</Th>
-                        <Th color="white">Score</Th> */}
-                    </Tr>
-                    
-                </Thead>
-                <Tbody>
-                    {/*     <Tr
-                            key={index}
-                            _hover={{ bg: "teal.50", boxShadow: "md" }}
-                            transition="background-color 0.3s, box-shadow 0.3s"
-                        >
-                            <Td>{article.id}</Td>
-                            <Td>{article.title}</Td>
-                            <Td>{article.author}</Td>
-                            <Td>{article.journal}</Td>
-                        </Tr>
-                    ))} */}
-
-                    {articles ? articles.map((e, index) => <Tr
+            <Thead bg="#263C56" borderRadius={10}>
+                <Tr>
+                    <Th w={'5rem'} textAlign='center' color="white">ID Paper</Th>
+                    <Th w={'5rem'} textAlign='center' color="white">Title</Th>
+                    <Th w={'5rem'} textAlign='center' color="white">Author</Th>
+                    <Th w='5rem' textAlign='center' color="white">Year</Th>
+                    <Th w='5rem' textAlign='center' color="white">Selection</Th>
+                    <Th w='5rem' textAlign='center' color="white">Extraction</Th>
+                    <Th w={'5rem'} textAlign='center'  color="white">Reading priority</Th>
+                    {/* 
+                    <Th color="white">Status/Selection</Th>
+                    <Th color="white">Status/Extraction</Th>
+                    <Th color="white">Reading Priority</Th>
+                    <Th color="white">Score</Th> */}
+                </Tr>
+                
+            </Thead>
+            <Tbody>
+                {/*     <Tr
                         key={index}
-                        _hover={{ bg: "#F5F8F9" }}
+                        _hover={{ bg: "teal.50", boxShadow: "md" }}
                         transition="background-color 0.3s, box-shadow 0.3s"
                     >
+                        <Td>{article.id}</Td>
+                        <Td>{article.title}</Td>
+                        <Td>{article.author}</Td>
+                        <Td>{article.journal}</Td>
+                    </Tr>
+                ))} */}
 
-                        <Td sx={collapsedTdSX}>{index + 1}</Td>
-                        
-                        <Td sx={collapsedTdSX}>
-                            <Tooltip label={e.title} aria-label="Título completo"
+                {articles ? articles.map((e, index) => <Tr
+                    key={index}
+                    _hover={{ bg: "#F5F8F9" }}
+                    transition="background-color 0.3s, box-shadow 0.3s"
+                >
+
+                    <Td sx={collapsedTdSX}>{index + 1}</Td>
+                    
+                    <Td sx={collapsedTdSX}>
+                        <Tooltip label={e.title} aria-label="Título completo"
+                        hasArrow
+                        placement="right" // Pode ser "top", "bottom", "left", "right"
+                        fontSize="xs" // Tamanho da fonte
+                        p={3} // Padding do tooltip>
+                        >
+                            <Text sx={collapsedTdSX}>{e.title}</Text>
+                        </Tooltip>
+                    </Td>
+
+                    <Td sx={collapsedTdSX}>
+                        <Tooltip label={e.authors} aria-label="Título completo"
                             hasArrow
                             placement="right" // Pode ser "top", "bottom", "left", "right"
                             fontSize="xs" // Tamanho da fonte
                             p={3} // Padding do tooltip>
                             >
-                                <Text sx={collapsedTdSX}>{e.title}</Text>
-                            </Tooltip>
-                        </Td>
+                                <Text sx={collapsedTdSX}>{e.authors}</Text>
+                        </Tooltip>
+                    </Td>
+                    <Td sx={collapsedTdSX}>
+                        <Tooltip label={e.year} aria-label="Título completo"
+                            hasArrow
+                            placement="right" // Pode ser "top", "bottom", "left", "right"
+                            fontSize="xs" // Tamanho da fonte
+                            p={3} // Padding do tooltip>
+                            >
+                                <Text sx={collapsedTdSX}>{e.year}</Text>
+                        </Tooltip>
+                    </Td>
+                    <Td sx={collapsedTdSX}>
+                        <Tooltip label={'accepted'} aria-label="Título completo"
+                            hasArrow
+                            placement="right" // Pode ser "top", "bottom", "left", "right"
+                            fontSize="xs" // Tamanho da fonte
+                            p={3} // Padding do tooltip>
+                            >
+                                <Text sx={collapsedTdSX}>{'accepted'}</Text>
+                        </Tooltip>
+                    </Td>
+                    <Td sx={collapsedTdSX}>
+                        <Tooltip label={'accepted'} aria-label="Título completo"
+                            hasArrow
+                            placement="right" // Pode ser "top", "bottom", "left", "right"
+                            fontSize="xs" // Tamanho da fonte
+                            p={3} // Padding do tooltip>
+                            >
+                                <Text sx={collapsedTdSX}>{'accepted'}</Text>
+                        </Tooltip>
+                    </Td>
+                    <Td sx={collapsedTdSX}>
+                        <Tooltip label={e.readingPriority} aria-label="Título completo"
+                            hasArrow
+                            placement="right" // Pode ser "top", "bottom", "left", "right"
+                            fontSize="xs" // Tamanho da fonte
+                            p={3} // Padding do tooltip>
+                            >
+                                <Text sx={collapsedTdSX}>{e.readingPriority}</Text>
+                        </Tooltip>
+                    </Td>
+                    
+                </Tr>) : <p>no articles Found</p>}
 
-                        <Td sx={collapsedTdSX}>
-                            <Tooltip label={e.authors} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={collapsedTdSX}>{e.authors}</Text>
-                            </Tooltip>
-                        </Td>
-                        <Td sx={collapsedTdSX}>
-                            <Tooltip label={e.year} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={collapsedTdSX}>{e.year}</Text>
-                            </Tooltip>
-                        </Td>
-                        <Td sx={collapsedTdSX}>
-                            <Tooltip label={'accepted'} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={collapsedTdSX}>{'accepted'}</Text>
-                            </Tooltip>
-                        </Td>
-                        <Td sx={collapsedTdSX}>
-                            <Tooltip label={'accepted'} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={collapsedTdSX}>{'accepted'}</Text>
-                            </Tooltip>
-                        </Td>
-                        <Td sx={collapsedTdSX}>
-                            <Tooltip label={e.readingPriority} aria-label="Título completo"
-                                hasArrow
-                                placement="right" // Pode ser "top", "bottom", "left", "right"
-                                fontSize="xs" // Tamanho da fonte
-                                p={3} // Padding do tooltip>
-                                >
-                                    <Text sx={collapsedTdSX}>{e.readingPriority}</Text>
-                            </Tooltip>
-                        </Td>
-                        
-                    </Tr>) : <p>no articles Found</p>}
-
-                </Tbody>
-            </Table>
-        </TableContainer>
-    )
+            </Tbody>
+        </Table>
+    </TableContainer>
   )
 }
