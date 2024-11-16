bca4b589a11c7e23df2d19ffaeec5e9c154a8a13
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-15 22:09:32 -0300

    fix: useFetchInclusionCriterias return

--- src/hooks/fetch/useFetchInclusionCriteria.tsx
+++ src/hooks/fetch/useFetchInclusionCriteria.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import axios from '../../interceptor/interceptor';
 import getRequestOptions from '../../utils/getRequestOptions';
 
@@ -5,12 +6,15 @@ const useGetInclusionCriteria = () => {
     const id = localStorage.getItem('systematicStudyId');
     const path = `http://localhost:8080/systematic-study/${id}/protocol`;
     const options = getRequestOptions();
+    const [inclusionCriterias, setInclusionCriterias] = useState<string[]>();
 
     axios.get(path, options)
         .then(res => {
-            return res.data.inclusionCriterias;
+            setInclusionCriterias(res.data.inclusionCriterias);
         })
         .catch(error => console.log(error + " Failed to get Inclusion Criterias"));
+        
+    return inclusionCriterias;
 }
 
 export default useGetInclusionCriteria;
